8f5ad6f4621e0e620c71dbbd275b04ebb0b2783a
Braden Steffaniak <BradenSteffaniak@gmail.com>
2023-10-31 07:30:00 -0400

    Add support for track_id ranges

--- packages/player/src/api.rs
+++ packages/player/src/api.rs
@@ -6,6 +6,7 @@ use actix_web::{
 use lambda_web::actix_web::{self, get, post};
 use moosicbox_core::app::AppState;
 use serde::Deserialize;
+use thiserror::Error;
 
 use crate::player::{
     next_track, pause_playback, play_track, play_tracks, player_status, previous_track,
@@ -68,6 +69,69 @@ pub struct PlayTracksQuery {
     pub seek: Option<f64>,
 }
 
+#[derive(Debug, Error)]
+pub enum ParseTrackIdsError {
+    #[error("Could not parse trackId: {0}")]
+    ParseId(String),
+    #[error("Could not parse trackId: {0}")]
+    UnmatchedRange(String),
+    #[error("Range too large: {0}")]
+    RangeTooLarge(String),
+}
+
+fn parse_track_id_sequences(track_ids: &str) -> std::result::Result<Vec<i32>, ParseTrackIdsError> {
+    track_ids
+        .split(',')
+        .map(|id| {
+            id.parse::<i32>()
+                .map_err(|_| ParseTrackIdsError::ParseId(id.into()))
+        })
+        .collect::<std::result::Result<Vec<_>, _>>()
+}
+
+fn parse_track_id_ranges(
+    track_id_ranges: &str,
+) -> std::result::Result<Vec<i32>, ParseTrackIdsError> {
+    let ranges = track_id_ranges.split('-').collect::<Vec<_>>();
+
+    if ranges.len() == 1 {
+        parse_track_id_sequences(ranges[0])
+    } else if ranges.len() > 2 && ranges.len() % 2 == 1 {
+        Err(ParseTrackIdsError::UnmatchedRange(track_id_ranges.into()))
+    } else {
+        let mut i = 0;
+        let mut ids = Vec::new();
+
+        while i < ranges.len() {
+            let mut start = parse_track_id_sequences(ranges[i])?;
+            let mut start_id = start[start.len() - 1] + 1;
+            let mut end = parse_track_id_sequences(ranges[i + 1])?;
+            let end_id = end[0];
+
+            if end_id - start_id > 100000 {
+                return Err(ParseTrackIdsError::RangeTooLarge(format!(
+                    "{}-{}",
+                    start_id - 1,
+                    end_id,
+                )));
+            }
+
+            ids.append(&mut start);
+
+            while start_id < end_id {
+                ids.push(start_id);
+                start_id += 1;
+            }
+
+            ids.append(&mut end);
+
+            i += 2;
+        }
+
+        Ok(ids)
+    }
+}
+
 #[post("/player/play/tracks")]
 pub async fn play_tracks_endpoint(
     query: web::Query<PlayTracksQuery>,
@@ -75,14 +139,17 @@ pub async fn play_tracks_endpoint(
 ) -> Result<Json<PlaybackStatus>> {
     Ok(Json(play_tracks(
         data.db.clone().expect("No DB bound on AppState"),
-        query
-            .track_ids
-            .split(",")
-            .map(|t| {
-                t.parse::<i32>()
-                    .map_err(|_| ErrorBadRequest(format!("Could not parse trackId '{t}'")))
-            })
-            .collect::<Result<Vec<_>, _>>()?,
+        parse_track_id_ranges(&query.track_ids).map_err(|e| match e {
+            ParseTrackIdsError::ParseId(id) => {
+                ErrorBadRequest(format!("Could not parse trackId '{id}'"))
+            }
+            ParseTrackIdsError::UnmatchedRange(range) => {
+                ErrorBadRequest(format!("Unmatched range '{range}'"))
+            }
+            ParseTrackIdsError::RangeTooLarge(range) => {
+                ErrorBadRequest(format!("Range too large '{range}'"))
+            }
+        })?,
         query.position,
         query.seek,
     )?))
