98a92d0df4d89787aea8c3b7974d1dd2068280b3
Braden Steffaniak <BradenSteffaniak@gmail.com>
2023-11-15 07:30:00 -0500

    Update client_id to be the primary key

--- migrations/mysql/2023-11-13-140925_create_connections_table/up.sql
+++ migrations/mysql/2023-11-13-140925_create_connections_table/up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS connections (
-    tunnel_ws_id VARCHAR(128) PRIMARY KEY NOT NULL,
-    client_id VARCHAR(128),
-    created TEXT DEFAULT (DATE_FORMAT(NOW(), '%Y-%m-%dT%H:%M:%f')),
-    updated TEXT DEFAULT (DATE_FORMAT(NOW(), '%Y-%m-%dT%H:%M:%f'))
+    client_id VARCHAR(64) PRIMARY KEY NOT NULL,
+    tunnel_ws_id VARCHAR(64) NOT NULL,
+    created TEXT DEFAULT (DATE_FORMAT(NOW(), '%Y-%m-%dT%H:%i:%f')),
+    updated TEXT DEFAULT (DATE_FORMAT(NOW(), '%Y-%m-%dT%H:%i:%f'))
 );
