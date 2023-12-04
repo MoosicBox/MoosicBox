b5cc36a338409937491407c5c7c6f27686d845c7
Braden Steffaniak <BradenSteffaniak@gmail.com>
2023-12-04 07:30:00 -0500

    Exit on failure

--- aws-deploy.sh
+++ aws-deploy.sh
@@ -4,7 +4,7 @@ SERVICE=$1
 ECR_REPOSITORY_NAME=$2
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 
-docker compose build "$SERVICE"
-docker tag "$SERVICE" "$AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/$ECR_REPOSITORY_NAME"
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin "$AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com"
-docker push "$AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/$ECR_REPOSITORY_NAME"
+docker compose build "$SERVICE" || exit 1
+docker tag "$SERVICE" "$AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/$ECR_REPOSITORY_NAME" || exit 1
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin "$AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com" || exit 1
+docker push "$AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/$ECR_REPOSITORY_NAME" || exit 1
