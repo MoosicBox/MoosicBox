27356a1c4890c7f6c3ce947e232981b5d05ca4d9
Braden Steffaniak <BradenSteffaniak@gmail.com>
2023-12-19 07:30:00 -0500

    Fix not deleting duplicate track_sizes before adding unique constraint

--- migrations/sqlite/2023-12-19-041015_fix_unique_constraint_to_track_sizes/up.sql
+++ migrations/sqlite/2023-12-19-041015_fix_unique_constraint_to_track_sizes/up.sql
@@ -1,4 +1,17 @@
 DROP INDEX IF EXISTS ux_track_sizes_props;
+DELETE FROM track_sizes
+WHERE rowid NOT IN (
+    SELECT MIN(rowid)
+    FROM track_sizes
+    GROUP BY
+        track_id,
+        ifnull(`format`, ''),
+        ifnull(`audio_bitrate`, 0),
+        ifnull(`overall_bitrate`, 0),
+        ifnull(`bit_depth`, 0),
+        ifnull(`sample_rate`, 0),
+        ifnull(`channels`, 0)
+);
 CREATE UNIQUE INDEX ux_track_sizes_props ON track_sizes(
     track_id,
     ifnull(`format`, ''),
