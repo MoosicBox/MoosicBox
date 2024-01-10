ec46b55a67e713529dfde38a454a817b01ee4970
Braden Steffaniak <BradenSteffaniak@gmail.com>
2024-01-10 07:30:00 -0500

    Add get_tests_dir_path

--- packages/config/src/lib.rs
+++ packages/config/src/lib.rs
@@ -9,3 +9,11 @@ pub fn get_config_dir_path() -> Option<PathBuf> {
 pub fn get_cache_dir_path() -> Option<PathBuf> {
     get_config_dir_path().map(|config| config.join("cache"))
 }
+
+#[cfg(feature = "test")]
+pub fn get_tests_dir_path() -> PathBuf {
+    std::env::temp_dir().join(format!(
+        "moosicbox_tests_{}",
+        rand::Rng::gen::<usize>(&mut rand::thread_rng())
+    ))
+}
