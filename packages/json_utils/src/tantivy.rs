b56c5549701fca878fa5c7ff2cb6e482f3770780
Braden Steffaniak <BradenSteffaniak@gmail.com>
2024-01-14 21:52:48 -0500

    Add support for Vec<T> return types for tantivy

--- packages/json_utils/src/tantivy.rs
+++ packages/json_utils/src/tantivy.rs
@@ -135,44 +135,42 @@ impl ToValueType<u64> for &Value {
     }
 }
 
-pub trait ToValue {
+pub trait ToValue<Type> {
     fn to_value<'a, T>(&'a self, index: &str) -> Result<T, ParseError>
     where
-        &'a Value: ToValueType<T>;
+        Type: 'a,
+        &'a Type: ToValueType<T>;
 }
 
-impl ToValue for NamedFieldDocument {
+impl ToValue<Vec<Value>> for NamedFieldDocument {
     fn to_value<'a, T>(&'a self, index: &str) -> Result<T, ParseError>
     where
-        &'a Value: ToValueType<T>,
+        Vec<Value>: 'a,
+        &'a Vec<Value>: ToValueType<T>,
     {
-        get_value_type(self, index)
+        get_doc_value_types(self, index)
     }
 }
 
-impl ToValue for &NamedFieldDocument {
+impl ToValue<Vec<Value>> for &NamedFieldDocument {
     fn to_value<'a, T>(&'a self, index: &str) -> Result<T, ParseError>
     where
-        &'a Value: ToValueType<T>,
+        Vec<Value>: 'a,
+        &'a Vec<Value>: ToValueType<T>,
     {
-        get_value_type(self, index)
+        get_doc_value_types(self, index)
     }
 }
 
-pub fn get_value_types<'a, T>(
+pub fn get_doc_value_types<'a, T>(
     value: &'a NamedFieldDocument,
     index: &str,
-) -> Result<Vec<T>, ParseError>
+) -> Result<T, ParseError>
 where
-    &'a Value: ToValueType<T>,
+    &'a Vec<Value>: ToValueType<T>,
 {
     if let Some(inner) = value.0.get(index) {
-        let inner = inner
-            .iter()
-            .map(|x| x.to_value_type())
-            .collect::<Result<Vec<_>, _>>()?;
-
-        return Ok(inner);
+        return inner.to_value_type();
     }
 
     Err(ParseError::Parse(format!("Missing value: '{}'", index)))
@@ -190,4 +188,150 @@ where
     }
 
     Err(ParseError::Parse(format!("Missing value: '{}'", index)))
+}
+
+impl<'a> ToValueType<&'a str> for &'a Vec<Value> {
+    fn to_value_type(self) -> Result<&'a str, ParseError> {
+        self.first()
+            .ok_or_else(|| ParseError::ConvertType("&str".into()))?
+            .to_value_type()
+    }
+
+    fn missing_value(self, error: ParseError) -> Result<&'a str, ParseError> {
+        Err(error)
+    }
+}
+
+impl<'a> ToValueType<&'a Vec<Value>> for &'a Vec<Value> {
+    fn to_value_type(self) -> Result<&'a Vec<Value>, ParseError> {
+        Ok(self)
+    }
+
+    fn missing_value(self, error: ParseError) -> Result<&'a Vec<Value>, ParseError> {
+        Err(error)
+    }
+}
+
+impl<'a, T> ToValueType<Vec<T>> for &'a Vec<Value>
+where
+    &'a Value: ToValueType<T>,
+{
+    fn to_value_type(self) -> Result<Vec<T>, ParseError> {
+        self.iter()
+            .map(|inner| inner.to_value_type())
+            .collect::<Result<Vec<_>, _>>()
+    }
+
+    fn missing_value(self, error: ParseError) -> Result<Vec<T>, ParseError> {
+        Err(error)
+    }
+}
+
+impl<'a, T> ToValueType<Option<T>> for &'a Vec<Value>
+where
+    &'a Vec<Value>: ToValueType<T>,
+{
+    fn to_value_type(self) -> Result<Option<T>, ParseError> {
+        self.to_value_type().map(|inner| Some(inner))
+    }
+
+    fn missing_value(self, _error: ParseError) -> Result<Option<T>, ParseError> {
+        Ok(None)
+    }
+}
+
+impl ToValueType<String> for &Vec<Value> {
+    fn to_value_type(self) -> Result<String, ParseError> {
+        self.first()
+            .ok_or_else(|| ParseError::ConvertType("String".into()))?
+            .to_value_type()
+    }
+
+    fn missing_value(self, error: ParseError) -> Result<String, ParseError> {
+        Err(error)
+    }
+}
+
+impl ToValueType<bool> for &Vec<Value> {
+    fn to_value_type(self) -> Result<bool, ParseError> {
+        self.first()
+            .ok_or_else(|| ParseError::ConvertType("bool".into()))?
+            .to_value_type()
+    }
+
+    fn missing_value(self, error: ParseError) -> Result<bool, ParseError> {
+        Err(error)
+    }
+}
+
+impl ToValueType<f32> for &Vec<Value> {
+    fn to_value_type(self) -> Result<f32, ParseError> {
+        self.first()
+            .ok_or_else(|| ParseError::ConvertType("f32".into()))?
+            .to_value_type()
+    }
+
+    fn missing_value(self, error: ParseError) -> Result<f32, ParseError> {
+        Err(error)
+    }
+}
+
+impl ToValueType<f64> for &Vec<Value> {
+    fn to_value_type(self) -> Result<f64, ParseError> {
+        self.first()
+            .ok_or_else(|| ParseError::ConvertType("f64".into()))?
+            .to_value_type()
+    }
+
+    fn missing_value(self, error: ParseError) -> Result<f64, ParseError> {
+        Err(error)
+    }
+}
+
+impl ToValueType<u8> for &Vec<Value> {
+    fn to_value_type(self) -> Result<u8, ParseError> {
+        self.first()
+            .ok_or_else(|| ParseError::ConvertType("u8".into()))?
+            .to_value_type()
+    }
+
+    fn missing_value(self, error: ParseError) -> Result<u8, ParseError> {
+        Err(error)
+    }
+}
+
+impl ToValueType<u16> for &Vec<Value> {
+    fn to_value_type(self) -> Result<u16, ParseError> {
+        self.first()
+            .ok_or_else(|| ParseError::ConvertType("u16".into()))?
+            .to_value_type()
+    }
+
+    fn missing_value(self, error: ParseError) -> Result<u16, ParseError> {
+        Err(error)
+    }
+}
+
+impl ToValueType<u32> for &Vec<Value> {
+    fn to_value_type(self) -> Result<u32, ParseError> {
+        self.first()
+            .ok_or_else(|| ParseError::ConvertType("u32".into()))?
+            .to_value_type()
+    }
+
+    fn missing_value(self, error: ParseError) -> Result<u32, ParseError> {
+        Err(error)
+    }
+}
+
+impl ToValueType<u64> for &Vec<Value> {
+    fn to_value_type(self) -> Result<u64, ParseError> {
+        self.first()
+            .ok_or_else(|| ParseError::ConvertType("u64".into()))?
+            .to_value_type()
+    }
+
+    fn missing_value(self, error: ParseError) -> Result<u64, ParseError> {
+        Err(error)
+    }
 }
