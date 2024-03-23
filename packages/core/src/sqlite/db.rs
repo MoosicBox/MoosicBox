433f37e66d08c34178d5199ecc593d49dde44987
Braden Steffaniak <BradenSteffaniak@gmail.com>
2024-03-23 17:27:47 -0400

    Fix NowAdd value

--- packages/core/src/sqlite/db.rs
+++ packages/core/src/sqlite/db.rs
@@ -168,7 +168,7 @@ pub async fn save_magic_token(
         .value("magic_token", magic_token)
         .value("access_token", access_token)
         .value("client_id", client_id)
-        .value("client_id", DatabaseValue::NowAdd("+1 Day".into()))
+        .value("expires", DatabaseValue::NowAdd("'+1 Day'".into()))
         .execute_first(db)
         .await?;
 
