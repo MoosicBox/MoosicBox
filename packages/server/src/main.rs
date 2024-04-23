ae64c65c1e4276dbbb6fa3f05b778c78c0452166
Braden Steffaniak <BradenSteffaniak@gmail.com>
2024-04-23 07:30:00 -0400

    Move tunnel setup code to specific mod & clean up clippy lints

--- packages/server/src/main.rs
+++ packages/server/src/main.rs
@@ -6,18 +6,15 @@ mod api;
 mod auth;
 mod db;
 mod playback_session;
+mod tunnel;
 mod ws;
 
 use actix_cors::Cors;
 use actix_web::{http, middleware, web, App};
-use log::{debug, error, info};
-use moosicbox_auth::get_client_id_and_access_token;
 use moosicbox_core::app::AppState;
 use moosicbox_database::Database;
 use moosicbox_downloader::{api::models::ApiProgressEvent, queue::ProgressEvent};
 use moosicbox_env_utils::{default_env, default_env_usize, option_env_usize};
-use moosicbox_tunnel::TunnelRequest;
-use moosicbox_tunnel_sender::{sender::TunnelSender, TunnelMessage};
 use moosicbox_ws::api::send_download_event;
 use once_cell::sync::Lazy;
 use std::{
@@ -28,7 +25,6 @@ use std::{
 use throttle::Throttle;
 use tokio::{task::spawn, try_join};
 use tokio_util::sync::CancellationToken;
-use url::Url;
 use ws::server::ChatServer;
 
 static CANCELLATION_TOKEN: Lazy<CancellationToken> = Lazy::new(CancellationToken::new);
@@ -36,9 +32,11 @@ static CANCELLATION_TOKEN: Lazy<CancellationToken> = Lazy::new(CancellationToken
 static CHAT_SERVER_HANDLE: Lazy<std::sync::RwLock<Option<ws::server::ChatServerHandle>>> =
     Lazy::new(|| std::sync::RwLock::new(None));
 
+#[allow(clippy::type_complexity)]
 static DB: Lazy<std::sync::RwLock<Option<Arc<Box<dyn Database>>>>> =
     Lazy::new(|| std::sync::RwLock::new(None));
 
+#[allow(clippy::too_many_lines)]
 fn main() -> std::io::Result<()> {
     #[cfg(debug_assertions)]
     const DEFAULT_LOG_LEVEL: &str = "moosicbox=trace";
@@ -86,12 +84,20 @@ fn main() -> std::io::Result<()> {
         let (db, db_connection) = db::init_postgres_raw_native_tls()
             .await
             .expect("Failed to init postgres DB");
-        #[cfg(all(not(feature = "postgres-native-tls"), feature = "postgres-openssl", feature = "postgres-raw"))]
+        #[cfg(all(
+            not(feature = "postgres-native-tls"),
+            feature = "postgres-openssl",
+            feature = "postgres-raw"
+        ))]
         #[allow(unused)]
         let (db, db_connection) = db::init_postgres_raw_openssl()
             .await
             .expect("Failed to init postgres DB");
-        #[cfg(all(not(feature = "postgres-native-tls"), not(feature = "postgres-openssl"), feature = "postgres-raw"))]
+        #[cfg(all(
+            not(feature = "postgres-native-tls"),
+            not(feature = "postgres-openssl"),
+            feature = "postgres-raw"
+        ))]
         #[allow(unused)]
         let (db, db_connection) = db::init_postgres_raw_no_tls()
             .await
@@ -112,21 +118,18 @@ fn main() -> std::io::Result<()> {
 
         moosicbox_downloader::api::add_progress_listener_to_download_queue(Box::new(
             move |event| {
-                let binding = CHAT_SERVER_HANDLE.read().unwrap_or_else(|e| e.into_inner());
-                let sender = binding.as_ref().unwrap();
-
                 let event = if let ProgressEvent::BytesRead { task, read, total } = event {
-                    if let Err(_) = bytes_throttle.lock().unwrap().accept() {
+                    if bytes_throttle.lock().unwrap().accept().is_err() {
                         bytes_buf.fetch_add(*read, std::sync::atomic::Ordering::SeqCst);
                         return;
-                    } else {
-                        let bytes = bytes_buf.load(std::sync::atomic::Ordering::SeqCst);
-                        bytes_buf.store(0, std::sync::atomic::Ordering::SeqCst);
-                        ProgressEvent::BytesRead {
-                            task: task.clone(),
-                            read: *read + bytes,
-                            total: *total,
-                        }
+                    }
+
+                    let bytes = bytes_buf.load(std::sync::atomic::Ordering::SeqCst);
+                    bytes_buf.store(0, std::sync::atomic::Ordering::SeqCst);
+                    ProgressEvent::BytesRead {
+                        task: task.clone(),
+                        read: *read + bytes,
+                        total: *total,
                     }
                 } else {
                     event.clone()
@@ -134,7 +137,15 @@ fn main() -> std::io::Result<()> {
 
                 let api_event: ApiProgressEvent = event.into();
 
-                if let Err(err) = send_download_event(sender, None, api_event) {
+                if let Err(err) = send_download_event(
+                    CHAT_SERVER_HANDLE
+                        .read()
+                        .unwrap_or_else(std::sync::PoisonError::into_inner)
+                        .as_ref()
+                        .unwrap(),
+                    None,
+                    api_event,
+                ) {
                     log::error!("Failed to broadcast download event: {err:?}");
                 }
             },
@@ -145,130 +156,12 @@ fn main() -> std::io::Result<()> {
         CHAT_SERVER_HANDLE.write().unwrap().replace(server_tx);
 
         #[cfg(feature = "postgres-raw")]
-        let db_connection_handle = tokio::spawn(async { db_connection.await });
+        let db_connection_handle = tokio::spawn(db_connection);
 
-        let (tunnel_host, tunnel_join_handle, tunnel_handle) = if let Ok(url) = env::var("WS_HOST")
-        {
-            if !url.is_empty() {
-                log::debug!("Using WS_HOST: {url}");
-                let ws_url = url.clone();
-                let url = Url::parse(&url).expect("Invalid WS_HOST");
-                let hostname = url
-                    .host_str()
-                    .map(|s| s.to_string())
-                    .expect("Invalid WS_HOST");
-                let host = format!(
-                    "{}://{hostname}{}",
-                    if url.scheme() == "wss" {
-                        "https"
-                    } else {
-                        "http"
-                    },
-                    if let Some(port) = url.port() {
-                        format!(":{port}")
-                    } else {
-                        "".to_string()
-                    }
-                );
-                // FIXME: Handle retry
-                let (client_id, access_token) = {
-                    get_client_id_and_access_token(&**database, &host)
-                        .await
-                        .map_err(|e| {
-                            std::io::Error::new(
-                                std::io::ErrorKind::Other,
-                                format!("Could not get access token: {e:?}"),
-                            )
-                        })?
-                };
-                let (mut tunnel, handle) =
-                    TunnelSender::new(host.clone(), ws_url, client_id, access_token);
-
-                tunnel = tunnel.with_cancellation_token(CANCELLATION_TOKEN.clone());
-
-                let database_send = database.clone();
-                (
-                    Some(host),
-                    Some(spawn(async move {
-                        let mut rx = tunnel.start();
-
-                        while let Some(m) = rx.recv().await {
-                            match m {
-                                TunnelMessage::Text(m) => {
-                                    debug!("Received text TunnelMessage {}", &m);
-                                    let tunnel = tunnel.clone();
-                                    let database_send = database_send.clone();
-                                    spawn(async move {
-                                        match serde_json::from_str(&m).unwrap() {
-                                            TunnelRequest::Http(request) => {
-                                                if let Err(err) = tunnel
-                                                    .tunnel_request(
-                                                        database_send.clone(),
-                                                        service_port,
-                                                        request.request_id,
-                                                        request.method,
-                                                        request.path,
-                                                        request.query,
-                                                        request.payload,
-                                                        request.headers,
-                                                        request.encoding,
-                                                    )
-                                                    .await
-                                                {
-                                                    log::error!("Tunnel request failed: {err:?}");
-                                                }
-                                            }
-                                            TunnelRequest::Ws(request) => {
-                                                let sender = CHAT_SERVER_HANDLE
-                                                    .read()
-                                                    .unwrap_or_else(|e| e.into_inner())
-                                                    .as_ref()
-                                                    .ok_or("Failed to get chat server handle")?
-                                                    .clone();
-                                                if let Err(err) = tunnel
-                                                    .ws_request(
-                                                        &**database_send,
-                                                        request.conn_id,
-                                                        request.request_id,
-                                                        request.body.clone(),
-                                                        sender,
-                                                    )
-                                                    .await
-                                                {
-                                                    error!(
-                                                        "Failed to propagate ws request {} from conn_id {}: {err:?}",
-                                                        request.request_id,
-                                                        request.conn_id
-                                                    );
-                                                }
-                                            }
-                                            TunnelRequest::Abort(request) => {
-                                                log::debug!("Aborting request {}", request.request_id);
-                                                tunnel.abort_request(request.request_id);
-                                            }
-                                        }
-                                        Ok::<_, String>(())
-                                    });
-                                }
-                                TunnelMessage::Binary(_) => todo!(),
-                                TunnelMessage::Ping(_) => {}
-                                TunnelMessage::Pong(_) => todo!(),
-                                TunnelMessage::Close => {
-                                    info!("Tunnel connection was closed");
-                                }
-                                TunnelMessage::Frame(_) => todo!(),
-                            }
-                        }
-                        debug!("Exiting tunnel message loop");
-                    })),
-                    Some(handle),
-                )
-            } else {
-                (None, None, None)
-            }
-        } else {
-            (None, None, None)
-        };
+        let (tunnel_host, tunnel_join_handle, tunnel_handle) =
+            crate::tunnel::setup_tunnel(database.clone(), service_port)
+                .await
+                .expect("Failed to setup tunnel connection");
 
         if let Some(ref tunnel_handle) = tunnel_handle {
             chat_server.add_sender(Box::new(tunnel_handle.clone()));
@@ -286,7 +179,11 @@ fn main() -> std::io::Result<()> {
             let cors = Cors::default()
                 .allow_any_origin()
                 .allowed_methods(vec!["GET", "POST", "OPTIONS", "DELETE", "PUT", "PATCH"])
-                .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT, http::header::CONTENT_TYPE])
+                .allowed_headers(vec![
+                    http::header::AUTHORIZATION,
+                    http::header::ACCEPT,
+                    http::header::CONTENT_TYPE,
+                ])
                 .supports_credentials()
                 .max_age(3600);
 
---
+++ packages/server/src/tunnel.rs
@@ -0,0 +1,152 @@
+use std::{env, sync::Arc};
+
+use moosicbox_auth::get_client_id_and_access_token;
+use moosicbox_database::Database;
+use moosicbox_tunnel::TunnelRequest;
+use moosicbox_tunnel_sender::{
+    sender::{TunnelSender, TunnelSenderHandle},
+    TunnelMessage,
+};
+use thiserror::Error;
+use tokio::{spawn, task::JoinHandle};
+use url::Url;
+
+use crate::{CANCELLATION_TOKEN, CHAT_SERVER_HANDLE};
+
+#[derive(Debug, Error)]
+pub enum SetupTunnelError {
+    #[error(transparent)]
+    IO(#[from] std::io::Error),
+}
+
+#[allow(clippy::too_many_lines, clippy::module_name_repetitions)]
+pub async fn setup_tunnel(
+    database: Arc<Box<dyn Database>>,
+    service_port: u16,
+) -> Result<
+    (
+        Option<String>,
+        Option<JoinHandle<()>>,
+        Option<TunnelSenderHandle>,
+    ),
+    SetupTunnelError,
+> {
+    if let Ok(url) = env::var("WS_HOST") {
+        if url.is_empty() {
+            Ok((None, None, None))
+        } else {
+            log::debug!("Using WS_HOST: {url}");
+            let ws_url = url.clone();
+            let url = Url::parse(&url).expect("Invalid WS_HOST");
+            let hostname = url
+                .host_str()
+                .map(std::string::ToString::to_string)
+                .expect("Invalid WS_HOST");
+            let host = format!(
+                "{}://{hostname}{}",
+                if url.scheme() == "wss" {
+                    "https"
+                } else {
+                    "http"
+                },
+                url.port()
+                    .map_or_else(String::new, |port| format!(":{port}"))
+            );
+            // FIXME: Handle retry
+            let (client_id, access_token) = {
+                get_client_id_and_access_token(&**database, &host)
+                    .await
+                    .map_err(|e| {
+                        std::io::Error::new(
+                            std::io::ErrorKind::Other,
+                            format!("Could not get access token: {e:?}"),
+                        )
+                    })?
+            };
+            let (mut tunnel, handle) =
+                TunnelSender::new(host.clone(), ws_url, client_id, access_token);
+
+            tunnel = tunnel.with_cancellation_token(CANCELLATION_TOKEN.clone());
+
+            let database_send = database.clone();
+            Ok((
+                Some(host),
+                Some(spawn(async move {
+                    let mut rx = tunnel.start();
+
+                    while let Some(m) = rx.recv().await {
+                        match m {
+                            TunnelMessage::Text(m) => {
+                                log::debug!("Received text TunnelMessage {}", &m);
+                                let tunnel = tunnel.clone();
+                                let database_send = database_send.clone();
+                                spawn(async move {
+                                    match serde_json::from_str(&m).unwrap() {
+                                        TunnelRequest::Http(request) => {
+                                            if let Err(err) = tunnel
+                                                .tunnel_request(
+                                                    database_send.clone(),
+                                                    service_port,
+                                                    request.request_id,
+                                                    request.method,
+                                                    request.path,
+                                                    request.query,
+                                                    request.payload,
+                                                    request.headers,
+                                                    request.encoding,
+                                                )
+                                                .await
+                                            {
+                                                log::error!("Tunnel request failed: {err:?}");
+                                            }
+                                        }
+                                        TunnelRequest::Ws(request) => {
+                                            let sender = CHAT_SERVER_HANDLE
+                                                .read()
+                                                .unwrap_or_else(std::sync::PoisonError::into_inner)
+                                                .as_ref()
+                                                .ok_or("Failed to get chat server handle")?
+                                                .clone();
+                                            if let Err(err) = tunnel
+                                                .ws_request(
+                                                    &**database_send,
+                                                    request.conn_id,
+                                                    request.request_id,
+                                                    request.body.clone(),
+                                                    sender,
+                                                )
+                                                .await
+                                            {
+                                                log::error!(
+                                                        "Failed to propagate ws request {} from conn_id {}: {err:?}",
+                                                        request.request_id,
+                                                        request.conn_id
+                                                    );
+                                            }
+                                        }
+                                        TunnelRequest::Abort(request) => {
+                                            log::debug!("Aborting request {}", request.request_id);
+                                            tunnel.abort_request(request.request_id);
+                                        }
+                                    }
+                                    Ok::<_, String>(())
+                                });
+                            }
+                            TunnelMessage::Binary(_) => todo!(),
+                            TunnelMessage::Ping(_) => {}
+                            TunnelMessage::Pong(_) => todo!(),
+                            TunnelMessage::Close => {
+                                log::info!("Tunnel connection was closed");
+                            }
+                            TunnelMessage::Frame(_) => todo!(),
+                        }
+                    }
+                    log::debug!("Exiting tunnel message loop");
+                })),
+                Some(handle),
+            ))
+        }
+    } else {
+        Ok((None, None, None))
+    }
+}
