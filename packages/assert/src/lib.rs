fa78a75a74d69b0291daf95add235bfebcfd12db
Braden Steffaniak <BradenSteffaniak@gmail.com>
2024-06-07 07:30:00 -0400

    Add die_or_error macro

--- packages/assert/src/lib.rs
+++ packages/assert/src/lib.rs
@@ -76,3 +76,36 @@ macro_rules! die {
         }
     };
 }
+
+#[macro_export]
+macro_rules! die_or_error {
+    ($($message:tt)+) => {
+        if $crate::moosicbox_env_utils::default_env!("ENABLE_ASSERT", "false") == "1" {
+            eprintln!(
+                "{}",
+                $crate::Colorize::on_red($crate::Colorize::white($crate::Colorize::bold(
+                    format!(
+                        "{}\n{}",
+                        $crate::Colorize::underline(format!($($message)*).as_str()),
+                        std::backtrace::Backtrace::force_capture()
+                    )
+                    .as_str()
+                )))
+            );
+            log::logger().flush();
+            std::process::exit(1);
+        } else {
+            log::error!(
+                "{}",
+                $crate::Colorize::on_red($crate::Colorize::white($crate::Colorize::bold(
+                    format!(
+                        "{}\n{}",
+                        $crate::Colorize::underline(format!($($message)*).as_str()),
+                        std::backtrace::Backtrace::force_capture()
+                    )
+                    .as_str()
+                )))
+            );
+        }
+    };
+}
