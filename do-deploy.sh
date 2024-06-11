995abffbcf357b18b8a2e2f6438dd9138d1fea74
Braden Steffaniak <BradenSteffaniak@gmail.com>
2024-06-11 07:30:00 -0400

    Run `kubectl apply` in do-deploy.sh

--- do-deploy.sh
+++ do-deploy.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
 SERVICE=$1
+KUBERNETES_DIRECTORY=$2
+[[ -z $KUBERNETES_DIRECTORY ]] && { echo "Must pass kubernetes directory as second argument" >&2; exit 1; }
 
 docker compose build "$SERVICE" || exit 1
 docker tag "$SERVICE" "registry.digitalocean.com/moosicbox/$SERVICE" || exit 1
 doctl registry login
 docker push "registry.digitalocean.com/moosicbox/$SERVICE" || exit 1
+(cd "$KUBERNETES_DIRECTORY"; kubectl apply -f .) || exit 1
 kubectl rollout restart "deployment/$SERVICE"
