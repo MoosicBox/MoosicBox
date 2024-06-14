f5ac67e39b6244641adccf3414faf43142e154d9
Braden Steffaniak <BradenSteffaniak@gmail.com>
2024-06-14 07:30:00 -0400

    Log success/failure on api request finished

--- packages/middleware/src/api_logger.rs
+++ packages/middleware/src/api_logger.rs
@@ -1,5 +1,5 @@
 use actix_web::dev::{forward_ready, Service, ServiceRequest, ServiceResponse, Transform};
-use futures_util::future::LocalBoxFuture;
+use futures_util::{future::LocalBoxFuture, FutureExt};
 use std::future::{ready, Ready};
 
 #[allow(clippy::module_name_repetitions)]
@@ -55,16 +55,30 @@ where
     forward_ready!(service);
 
     fn call(&self, req: ServiceRequest) -> Self::Future {
-        log::trace!(
-            "{} {}{}",
-            req.method(),
-            req.path(),
-            if req.query_string().is_empty() {
+        let prefix = format!(
+            "{method} {path}{query}",
+            method = req.method(),
+            path = req.path(),
+            query = if req.query_string().is_empty() {
                 String::new()
             } else {
                 format!("?{}", req.query_string())
-            }
+            },
         );
-        Box::pin(self.service.call(req))
+        let start = std::time::Instant::now();
+        log::trace!("{prefix} STARTED");
+        Box::pin(self.service.call(req).then(move |response| async move {
+            let duration = std::time::Instant::now().duration_since(start).as_millis();
+            match response {
+                Ok(data) => {
+                    log::trace!("{prefix} FINISHED SUCCESS ({duration} ms)");
+                    Ok(data)
+                }
+                Err(e) => {
+                    log::trace!("{prefix} FINISHED FAILURE ({duration} ms): {e:?}");
+                    Err(e)
+                }
+            }
+        }))
     }
 }
