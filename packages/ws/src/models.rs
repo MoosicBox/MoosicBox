8173e8e296e05022528d7fc370aadb3aad00c127
Braden Steffaniak <BradenSteffaniak@gmail.com>
2024-07-31 07:30:00 -0400

    #6 Add InboundMessagePayload implementation

--- packages/ws/src/models.rs
+++ packages/ws/src/models.rs
@@ -1,8 +1,11 @@
 use moosicbox_core::sqlite::models::SetSeek;
-use moosicbox_session::models::{ApiConnection, ApiSession, ApiUpdateSession};
+use moosicbox_session::models::{
+    ApiConnection, ApiSession, ApiUpdateSession, CreateSession, DeleteSession, RegisterConnection,
+    RegisterPlayer, SetSessionActivePlayers, UpdateSession,
+};
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
-use strum_macros::EnumString;
+use strum_macros::{AsRefStr, EnumString};
 
 #[derive(Debug, Serialize, Deserialize, EnumString)]
 #[serde(rename_all = "SCREAMING_SNAKE_CASE")]
@@ -13,7 +16,7 @@ pub enum EventType {
     Message,
 }
 
-#[derive(Debug, Serialize, Deserialize, EnumString)]
+#[derive(Debug, Serialize, Deserialize, EnumString, AsRefStr)]
 #[serde(rename_all = "SCREAMING_SNAKE_CASE")]
 #[strum(serialize_all = "SCREAMING_SNAKE_CASE")]
 pub enum InboundMessageType {
@@ -36,8 +39,72 @@ impl std::fmt::Display for InboundMessageType {
     }
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 #[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+#[serde(tag = "type")]
+pub enum InboundMessagePayload {
+    Ping(EmptyPayload),
+    GetConnectionId(EmptyPayload),
+    GetSessions(EmptyPayload),
+    CreateSession(CreateSessionPayload),
+    UpdateSession(UpdateSessionPayload),
+    DeleteSession(DeleteSessionPayload),
+    RegisterConnection(RegisterConnectionPayload),
+    RegisterPlayers(RegisterPlayersPayload),
+    SetActivePlayers(SetActivePlayersPayload),
+    PlaybackAction(PlaybackActionPayload),
+    SetSeek(SetSeekPayload),
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct EmptyPayload {}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct CreateSessionPayload {
+    pub payload: CreateSession,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct UpdateSessionPayload {
+    pub payload: UpdateSession,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct DeleteSessionPayload {
+    pub payload: DeleteSession,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct RegisterConnectionPayload {
+    pub payload: RegisterConnection,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct RegisterPlayersPayload {
+    pub payload: Vec<RegisterPlayer>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct SetActivePlayersPayload {
+    pub payload: SetSessionActivePlayers,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct PlaybackActionPayload {
+    pub payload: Value,
+}
+
+#[derive(Debug, Serialize, Deserialize, EnumString, AsRefStr)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+#[strum(serialize_all = "SCREAMING_SNAKE_CASE")]
 pub enum OutboundMessageType {
     Connect,
     NewConnection,
