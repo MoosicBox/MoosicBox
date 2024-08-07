0ec6bdeb66082cd92e235e3a1857c5f1992b93ce
Braden Steffaniak <BradenSteffaniak@gmail.com>
2024-08-07 07:30:00 -0400

    #7 Add TryFromAsync trait

--- packages/core/src/lib.rs
+++ packages/core/src/lib.rs
@@ -1,6 +1,18 @@
 #![cfg_attr(feature = "fail-on-warnings", deny(warnings))]
 
+use async_trait::async_trait;
+
 pub mod app;
 pub mod integer_range;
 pub mod sqlite;
 pub mod types;
+
+#[async_trait]
+pub trait TryFromAsync<T>
+where
+    Self: Sized,
+{
+    type Error;
+
+    async fn try_from_async(value: T) -> Result<Self, Self::Error>;
+}
