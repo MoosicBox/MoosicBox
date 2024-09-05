a926fca6c4de031f3df11dea23914865a9973e64
Braden Steffaniak <BradenSteffaniak@gmail.com>
2024-09-04 20:41:51 -0400

    Move download event handling to own mod

---
+++ packages/server/src/download_event.rs
@@ -0,0 +1,53 @@
+use moosicbox_async_service::Arc;
+
+use crate::WS_SERVER_HANDLE;
+
+pub async fn init() {
+    let bytes_throttle = Arc::new(std::sync::Mutex::new(throttle::Throttle::new(
+        std::time::Duration::from_millis(200),
+        1,
+    )));
+    let bytes_buf = Arc::new(std::sync::atomic::AtomicUsize::new(0));
+
+    moosicbox_downloader::api::add_progress_listener_to_download_queue(Box::new(move |event| {
+        let bytes_throttle = bytes_throttle.clone();
+        let bytes_buf = bytes_buf.clone();
+        let event = event.clone();
+        Box::pin(async move {
+            let event = if let moosicbox_downloader::queue::ProgressEvent::BytesRead {
+                task,
+                read,
+                total,
+            } = event
+            {
+                if bytes_throttle.lock().unwrap().accept().is_err() {
+                    bytes_buf.fetch_add(read, std::sync::atomic::Ordering::SeqCst);
+                    return;
+                }
+
+                let bytes = bytes_buf.load(std::sync::atomic::Ordering::SeqCst);
+                bytes_buf.store(0, std::sync::atomic::Ordering::SeqCst);
+                moosicbox_downloader::queue::ProgressEvent::BytesRead {
+                    task,
+                    read: read + bytes,
+                    total,
+                }
+            } else {
+                event.clone()
+            };
+
+            let api_event: moosicbox_downloader::api::models::ApiProgressEvent = event.into();
+
+            if let Err(err) = moosicbox_ws::send_download_event(
+                WS_SERVER_HANDLE.read().await.as_ref().unwrap(),
+                None,
+                api_event,
+            )
+            .await
+            {
+                log::error!("Failed to broadcast download event: {err:?}");
+            }
+        }) as moosicbox_downloader::queue::ProgressListenerRefFut
+    }))
+    .await;
+}
--- packages/server/src/main.rs
+++ packages/server/src/main.rs
@@ -5,6 +5,8 @@ mod api;
 #[cfg(feature = "static-token-auth")]
 mod auth;
 mod db;
+#[cfg(feature = "downloader")]
+mod download_event;
 #[cfg(feature = "player")]
 mod playback_session;
 mod tunnel;
@@ -173,58 +175,8 @@ fn main() -> std::io::Result<()> {
             .replace(library_api_state.clone());
 
         #[cfg(feature = "downloader")]
-        let bytes_throttle = Arc::new(std::sync::Mutex::new(throttle::Throttle::new(
-            std::time::Duration::from_millis(200),
-            1,
-        )));
-        #[cfg(feature = "downloader")]
-        let bytes_buf = Arc::new(std::sync::atomic::AtomicUsize::new(0));
+        download_event::init().await;
 
-        #[cfg(feature = "downloader")]
-        moosicbox_downloader::api::add_progress_listener_to_download_queue(Box::new(
-            move |event| {
-                let bytes_throttle = bytes_throttle.clone();
-                let bytes_buf = bytes_buf.clone();
-                let event = event.clone();
-                Box::pin(async move {
-                    let event = if let moosicbox_downloader::queue::ProgressEvent::BytesRead {
-                        task,
-                        read,
-                        total,
-                    } = event
-                    {
-                        if bytes_throttle.lock().unwrap().accept().is_err() {
-                            bytes_buf.fetch_add(read, std::sync::atomic::Ordering::SeqCst);
-                            return;
-                        }
-
-                        let bytes = bytes_buf.load(std::sync::atomic::Ordering::SeqCst);
-                        bytes_buf.store(0, std::sync::atomic::Ordering::SeqCst);
-                        moosicbox_downloader::queue::ProgressEvent::BytesRead {
-                            task,
-                            read: read + bytes,
-                            total,
-                        }
-                    } else {
-                        event.clone()
-                    };
-
-                    let api_event: moosicbox_downloader::api::models::ApiProgressEvent =
-                        event.into();
-
-                    if let Err(err) = moosicbox_ws::send_download_event(
-                        WS_SERVER_HANDLE.read().await.as_ref().unwrap(),
-                        None,
-                        api_event,
-                    )
-                    .await
-                    {
-                        log::error!("Failed to broadcast download event: {err:?}");
-                    }
-                }) as moosicbox_downloader::queue::ProgressListenerRefFut
-            },
-        ))
-        .await;
 
         let (mut ws_server, server_tx) = WsServer::new(database.clone());
         #[cfg(feature = "player")]
