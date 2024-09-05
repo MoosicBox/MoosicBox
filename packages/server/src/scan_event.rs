3380da5fd2d58d3668abbda94f7f89db2d2ba4e4
Braden Steffaniak <BradenSteffaniak@gmail.com>
2024-09-05 07:30:00 -0400

    Throttle scan events

--- packages/server/src/scan_event.rs
+++ packages/server/src/scan_event.rs
@@ -1,23 +1,42 @@
+use moosicbox_async_service::Arc;
+use moosicbox_scan::event::{
+    add_progress_listener, ApiProgressEvent, ProgressEvent, ProgressListenerRefFut,
+};
+
 use crate::WS_SERVER_HANDLE;
 
 pub async fn init() {
-    moosicbox_scan::event::add_progress_listener(Box::new(move |event| {
+    let scan_throttle = Arc::new(std::sync::Mutex::new(throttle::Throttle::new(
+        std::time::Duration::from_millis(200),
+        1,
+    )));
+
+    add_progress_listener(Box::new(move |event| {
+        let scan_throttle = scan_throttle.clone();
         let event = event.clone();
 
         Box::pin(async move {
-            let api_event: Option<moosicbox_scan::event::ApiProgressEvent> = event.into();
-            if let Some(api_event) = api_event {
-                if let Err(err) = moosicbox_ws::send_scan_event(
-                    WS_SERVER_HANDLE.read().await.as_ref().unwrap(),
-                    None,
-                    api_event,
-                )
-                .await
-                {
-                    log::error!("Failed to broadcast scan event: {err:?}");
+            match &event {
+                ProgressEvent::ScanCountUpdated { .. } | ProgressEvent::ItemScanned { .. } => {
+                    if scan_throttle.lock().unwrap().accept().is_err() {
+                        return;
+                    }
+                    let api_event: Option<ApiProgressEvent> = event.into();
+                    if let Some(api_event) = api_event {
+                        if let Err(err) = moosicbox_ws::send_scan_event(
+                            WS_SERVER_HANDLE.read().await.as_ref().unwrap(),
+                            None,
+                            api_event,
+                        )
+                        .await
+                        {
+                            log::error!("Failed to broadcast scan event: {err:?}");
+                        }
+                    }
                 }
+                ProgressEvent::State { .. } => {}
             }
-        }) as moosicbox_scan::event::ProgressListenerRefFut
+        }) as ProgressListenerRefFut
     }))
     .await;
 }
