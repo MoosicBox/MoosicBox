3a331314484e61dc6b93846fbf6e4bfa2d9e2521
Braden Steffaniak <BradenSteffaniak@gmail.com>
2024-09-06 15:54:24 -0400

    MoosicBox/MoosicBoxUI#2 Add mdns registration capabilities

---
+++ packages/mdns_sd/src/lib.rs
@@ -0,0 +1,37 @@
+#![cfg_attr(feature = "fail-on-warnings", deny(warnings))]
+
+use mdns_sd::{ServiceDaemon, ServiceInfo};
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum RegisterServiceError {
+    #[error(transparent)]
+    MdnsSd(#[from] mdns_sd::Error),
+    #[error(transparent)]
+    IO(#[from] std::io::Error),
+}
+
+pub fn register_service(
+    instance_name: &str,
+    ip: &str,
+    port: u16,
+) -> Result<(), RegisterServiceError> {
+    let mdns = ServiceDaemon::new()?;
+    let service_type = "_moosicboxserver._tcp.local.";
+    let host_name = format!(
+        "{}.local.",
+        hostname::get()?
+            .into_string()
+            .unwrap_or("unknown".to_string())
+    );
+
+    log::debug!("register_service: Registering mdns service service_type={service_type} instance_name={instance_name} host_name={host_name} ip={ip} port={port}");
+
+    let service_info = ServiceInfo::new(service_type, instance_name, &host_name, ip, port, None)?;
+
+    mdns.register(service_info)?;
+
+    log::debug!("register_service: Registered mdns service service_type={service_type} instance_name={instance_name} host_name={host_name} ip={ip} port={port}");
+
+    Ok(())
+}
