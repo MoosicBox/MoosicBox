a67a554de5f67f53409f487ad4b53207a7febefc
Braden Steffaniak <BradenSteffaniak@gmail.com>
2024-09-07 12:28:03 -0400

    Allow spawning task on specified runtime/local sets

--- packages/task/src/lib.rs
+++ packages/task/src/lib.rs
@@ -6,21 +6,7 @@ where
     Fut: futures::Future + Send + 'static,
     Fut::Output: Send + 'static,
 {
-    log::trace!("spawn start: {name}");
-    #[cfg(debug_assertions)]
-    let future = {
-        let name = name.to_owned();
-        async move {
-            let response = future.await;
-            log::trace!("spawn finished: {name}");
-
-            response
-        }
-    };
-    tokio::task::Builder::new()
-        .name(name)
-        .spawn(future)
-        .unwrap()
+    spawn_on(name, &tokio::runtime::Handle::current(), future)
 }
 
 #[cfg(not(tokio_unstable))]
@@ -29,18 +15,7 @@ where
     Fut: futures::Future + Send + 'static,
     Fut::Output: Send + 'static,
 {
-    log::trace!("spawn start: {name}");
-    #[cfg(debug_assertions)]
-    let future = {
-        let name = name.to_owned();
-        async move {
-            let response = future.await;
-            log::trace!("spawn finished: {name}");
-
-            response
-        }
-    };
-    tokio::task::spawn(future)
+    spawn_on(name, &tokio::runtime::Handle::current(), future)
 }
 
 #[cfg(tokio_unstable)]
@@ -52,21 +27,7 @@ where
     Function: FnOnce() -> Output + Send + 'static,
     Output: Send + 'static,
 {
-    log::trace!("spawn_blocking start: {name}");
-    #[cfg(debug_assertions)]
-    let function = {
-        let name = name.to_owned();
-        move || {
-            let response = function();
-            log::trace!("spawn_blocking finished: {name}");
-
-            response
-        }
-    };
-    tokio::task::Builder::new()
-        .name(name)
-        .spawn_blocking(function)
-        .unwrap()
+    spawn_blocking_on(name, &tokio::runtime::Handle::current(), function)
 }
 
 #[cfg(not(tokio_unstable))]
@@ -78,18 +39,7 @@ where
     Function: FnOnce() -> Output + Send + 'static,
     Output: Send + 'static,
 {
-    log::trace!("spawn_blocking start: {name}");
-    #[cfg(debug_assertions)]
-    let function = {
-        let name = name.to_owned();
-        move || {
-            let response = function();
-            log::trace!("spawn_blocking finished: {name}");
-
-            response
-        }
-    };
-    tokio::task::spawn_blocking(function)
+    spawn_blocking_on(name, &tokio::runtime::Handle::current(), function)
 }
 
 #[cfg(tokio_unstable)]
@@ -133,4 +83,157 @@ where
         }
     };
     tokio::task::spawn_local(future)
+}
+
+#[cfg(tokio_unstable)]
+pub fn spawn_on<Fut>(
+    name: &str,
+    handle: &tokio::runtime::Handle,
+    future: Fut,
+) -> tokio::task::JoinHandle<Fut::Output>
+where
+    Fut: futures::Future + Send + 'static,
+    Fut::Output: Send + 'static,
+{
+    log::trace!("spawn start: {name}");
+    #[cfg(debug_assertions)]
+    let future = {
+        let name = name.to_owned();
+        async move {
+            let response = future.await;
+            log::trace!("spawn finished: {name}");
+
+            response
+        }
+    };
+    tokio::task::Builder::new()
+        .name(name)
+        .spawn_on(future, handle)
+        .unwrap()
+}
+
+#[cfg(not(tokio_unstable))]
+pub fn spawn_on<Fut>(
+    name: &str,
+    handle: &tokio::runtime::Handle,
+    future: Fut,
+) -> tokio::task::JoinHandle<Fut::Output>
+where
+    Fut: futures::Future + Send + 'static,
+    Fut::Output: Send + 'static,
+{
+    log::trace!("spawn start: {name}");
+    #[cfg(debug_assertions)]
+    let future = {
+        let name = name.to_owned();
+        async move {
+            let response = future.await;
+            log::trace!("spawn finished: {name}");
+
+            response
+        }
+    };
+    handle.spawn(future)
+}
+
+#[cfg(tokio_unstable)]
+pub fn spawn_blocking_on<Function, Output>(
+    name: &str,
+    handle: &tokio::runtime::Handle,
+    function: Function,
+) -> tokio::task::JoinHandle<Output>
+where
+    Function: FnOnce() -> Output + Send + 'static,
+    Output: Send + 'static,
+{
+    log::trace!("spawn_blocking start: {name}");
+    #[cfg(debug_assertions)]
+    let function = {
+        let name = name.to_owned();
+        move || {
+            let response = function();
+            log::trace!("spawn_blocking finished: {name}");
+
+            response
+        }
+    };
+    tokio::task::Builder::new()
+        .name(name)
+        .spawn_blocking_on(function, handle)
+        .unwrap()
+}
+
+#[cfg(not(tokio_unstable))]
+pub fn spawn_blocking_on<Function, Output>(
+    name: &str,
+    handle: &tokio::runtime::Handle,
+    function: Function,
+) -> tokio::task::JoinHandle<Output>
+where
+    Function: FnOnce() -> Output + Send + 'static,
+    Output: Send + 'static,
+{
+    log::trace!("spawn_blocking start: {name}");
+    #[cfg(debug_assertions)]
+    let function = {
+        let name = name.to_owned();
+        move || {
+            let response = function();
+            log::trace!("spawn_blocking finished: {name}");
+
+            response
+        }
+    };
+    handle.spawn_blocking(function)
+}
+
+#[cfg(tokio_unstable)]
+pub fn spawn_local_on<Fut>(
+    name: &str,
+    local_set: &tokio::task::LocalSet,
+    future: Fut,
+) -> tokio::task::JoinHandle<Fut::Output>
+where
+    Fut: futures::Future + 'static,
+    Fut::Output: 'static,
+{
+    log::trace!("spawn_local start: {name}");
+    #[cfg(debug_assertions)]
+    let future = {
+        let name = name.to_owned();
+        async move {
+            let response = future.await;
+            log::trace!("spawn_local finished: {name}");
+
+            response
+        }
+    };
+    tokio::task::Builder::new()
+        .name(name)
+        .spawn_local_on(future, local_set)
+        .unwrap()
+}
+
+#[cfg(not(tokio_unstable))]
+pub fn spawn_local_on<Fut>(
+    name: &str,
+    local_set: &tokio::task::LocalSet,
+    future: Fut,
+) -> tokio::task::JoinHandle<Fut::Output>
+where
+    Fut: futures::Future + 'static,
+    Fut::Output: 'static,
+{
+    log::trace!("spawn_local start: {name}");
+    #[cfg(debug_assertions)]
+    let future = {
+        let name = name.to_owned();
+        async move {
+            let response = future.await;
+            log::trace!("spawn_local finished: {name}");
+
+            response
+        }
+    };
+    local_set.spawn_local(future)
 }
