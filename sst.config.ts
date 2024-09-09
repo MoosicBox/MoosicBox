a78e3d6494b34bac3361b74c31094c9c4b9107a8
Braden Steffaniak <BradenSteffaniak@gmail.com>
2024-09-09 15:30:20 -0400

    #48 Format sst.config.ts

--- sst.config.ts
+++ sst.config.ts
@@ -1,22 +1,22 @@
 /// <reference path="./.sst/platform/config.d.ts" />
-import { readdirSync } from "fs";
+import { readdirSync } from 'fs';
 export default $config({
-  app(input) {
-    return {
-      name: "moosicbox",
-      removal: input?.stage === "prod" ? "retain" : "remove",
-      home: "aws",
-      providers: { digitalocean: "4.31.1", kubernetes: "4.17.1" },
-    };
-  },
-  async run() {
-    const outputs = {};
-    for (const value of readdirSync("./infra/")) {
-      const result = await import(`./infra/${value}`);
-      if (result.outputs) {
-        Object.assign(outputs, result.outputs);
-      }
-    }
-    return outputs;
-  },
+    app(input) {
+        return {
+            name: 'moosicbox',
+            removal: input?.stage === 'prod' ? 'retain' : 'remove',
+            home: 'aws',
+            providers: { digitalocean: '4.31.1', kubernetes: '4.17.1' },
+        };
+    },
+    async run() {
+        const outputs = {};
+        for (const value of readdirSync('./infra/')) {
+            const result = await import(`./infra/${value}`);
+            if (result.outputs) {
+                Object.assign(outputs, result.outputs);
+            }
+        }
+        return outputs;
+    },
 });
