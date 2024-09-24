15f4fabb4b1d63cb7c207cb5831a9e460e261d56
Braden Steffaniak <BradenSteffaniak@gmail.com>
2024-09-24 13:06:53 -0400

    Create new kubernetes cluster if prod one doesnt exist

--- infra/cluster.ts
+++ infra/cluster.ts
@@ -1,30 +1,31 @@
-const existingCluster = await digitalocean.getKubernetesCluster({
-    name: 'moosicbox-prod',
-});
+function newCluster() {
+    const example = digitalocean.getKubernetesVersions({});
+
+    return new digitalocean.KubernetesCluster(
+        'MoosicBox',
+        {
+            name: 'moosicbox-prod',
+            ha: false,
+            version: example.then((example) => example.latestVersion),
+            region: digitalocean.Region.NYC1,
+            nodePool: {
+                name: 'moosicbox-prod-pool',
+                autoScale: false,
+                size: digitalocean.DropletSlug.DropletS1VCPU2GB,
+                nodeCount: 1,
+                minNodes: 1,
+                maxNodes: 2,
+            },
+        },
+        {
+            retainOnDelete: true,
+        },
+    );
+}
 
 export const cluster =
     $app.stage === 'prod'
-        ? (existingCluster ??
-          new digitalocean.KubernetesCluster(
-              'MoosicBox',
-              {
-                  name: 'moosicbox-prod',
-                  ha: false,
-                  version: '1.30.4-do.0',
-                  region: digitalocean.Region.NYC1,
-                  nodePool: {
-                      name: 'moosicbox-prod-pool',
-                      autoScale: false,
-                      size: digitalocean.DropletSlug.DropletS1VCPU2GB,
-                      nodeCount: 1,
-                      minNodes: 1,
-                      maxNodes: 2,
-                  },
-              },
-              {
-                  retainOnDelete: true,
-              },
-          ))
+        ? newCluster()
         : await digitalocean.getKubernetesCluster({ name: 'moosicbox-prod' });
 
 export const kubeconfig = cluster.kubeConfigs[0].rawConfig;
