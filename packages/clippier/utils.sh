974b75517b6ca365e8ecffbfba6eb995265ad089
Braden Steffaniak <BradenSteffaniak@gmail.com>
2024-09-27 11:36:38 -0400

    Add better logging for clippier sh output

--- packages/clippier/utils.sh
+++ packages/clippier/utils.sh
@@ -2,8 +2,16 @@
 
 function cargo_each_feature() {
     while read -r feature; do
-        echo "cargo $* --features \"$feature\""
-        cargo "$@" --features "$feature" || return 1
+        local command="cargo $* --features \"$feature\""
+
+        echo "RUNNING \`$command\`"
+
+        if cargo "$@" --features "$feature"; then
+            echo "FINISHED \`$command\`"
+        else
+            >&2 echo "FAILED \`$command\`"
+            return 1
+        fi
     done <<<"$(moosicbox_clippier Cargo.toml)"
 
     echo "done"
