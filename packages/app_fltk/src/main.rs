c1c1c843bc0f1ab5f9dffac954c4f23083d5014c
Braden Steffaniak <BradenSteffaniak@gmail.com>
2024-09-26 23:16:01 -0400

    #71 Use basic routing

--- packages/app_fltk/src/main.rs
+++ packages/app_fltk/src/main.rs
@@ -1,16 +1,30 @@
-use moosicbox_htmx_transformer::ElementList;
-
 const WIDTH: u16 = 1000;
 const HEIGHT: u16 = 600;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     moosicbox_logging::init(None)?;
 
-    let mut renderer = moosicbox_renderer_fltk::Renderer::new(WIDTH, HEIGHT)?;
+    let mut renderer = moosicbox_renderer_fltk::Renderer::new(WIDTH, HEIGHT)?
+        .with_route("/", || {
+            moosicbox_app_fltk_ui::home()
+                .into_string()
+                .try_into()
+                .unwrap()
+        })
+        .with_route("/home", || {
+            moosicbox_app_fltk_ui::home()
+                .into_string()
+                .try_into()
+                .unwrap()
+        })
+        .with_route("/albums", || {
+            moosicbox_app_fltk_ui::home()
+                .into_string()
+                .try_into()
+                .unwrap()
+        });
 
-    let elements: ElementList = moosicbox_app_fltk_ui::home().into_string().try_into()?;
-
-    renderer.render(elements)?;
+    renderer.navigate("/")?;
 
     renderer.run()?;
 
