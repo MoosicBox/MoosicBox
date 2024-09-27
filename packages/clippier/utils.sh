a9c3b9c5c4b28941f0797f06a445e5fcf6b9381d
Braden Steffaniak <BradenSteffaniak@gmail.com>
2024-09-27 13:05:07 -0400

    Add cargo_each_package_each_feature function

--- packages/clippier/utils.sh
+++ packages/clippier/utils.sh
@@ -16,3 +16,27 @@ function cargo_each_feature() {
 
     echo "done"
 }
+
+function cargo_each_package_each_feature() {
+    while read -r package; do
+        local command="cargo_each_feature $*"
+        local dir="${package/Cargo.toml/}"
+
+        if ! (
+            cd "$dir" || return 1
+
+            echo "IN \`$dir\` RUNNING \`$command\`"
+
+            if cargo_each_feature "$@"; then
+                echo "IN \`$dir\` FINISHED \`$command\`"
+            else
+                >&2 echo "IN \`$dir\` FAILED \`$command\`"
+                return 1
+            fi
+        ); then
+            return 1
+        fi
+    done <<<"$(find packages -name 'Cargo.toml')"
+
+    echo "done"
+}
