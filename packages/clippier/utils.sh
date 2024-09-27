a4becdc904192424a1eeca72c4de6dedfe91a692
Braden Steffaniak <BradenSteffaniak@gmail.com>
2024-09-27 16:46:51 -0400

    Update cargo_each_feature_permutation to be more correct

--- packages/clippier/utils.sh
+++ packages/clippier/utils.sh
@@ -21,21 +21,48 @@ function cargo_each_feature() {
 
 function cargo_each_feature_permutation() {
     local ignore=("fail-on-warnings")
+    local args=("$@")
+    local all_features=()
+
+    while read -r feature; do
+        if [[ "$feature" == "default" ]] || [[ "$feature" == "fail-on-warnings" ]]; then
+            continue
+        fi
+
+        all_features+=("$feature")
+    done <<<"$(moosicbox_clippier Cargo.toml)"
 
     function cargo_each_feature_permutation_inner() {
-        local count=$1
-        local end=$((count))
-        local features=("${@:2:$end}")
-        local offset=$((end + 2))
+        local feature_count=$1
+        local features=("${@:2:$feature_count}")
+        local feature_offset=$((feature_count + 2))
+        local ignore_count="${*:$feature_offset:1}"
+        local ignore_offset=$((feature_offset + 1))
+        local ignore_features=("${@:$ignore_offset:$ignore_count}")
 
-        while read -r feature; do
-            if [[ "$feature" == "fail-on-warnings" ]]; then
-                continue
-            fi
-            if [[ "$feature" == "default" ]]; then
-                continue
-            fi
+        local features_string=""
 
+        for x in "${features[@]}"; do
+            if [[ -n "$features_string" ]]; then
+                features_string="${features_string},"
+            fi
+            features_string="${features_string}${x}"
+        done
+
+        local command="cargo ${args[*]} --features \"$features_string\""
+
+        echo "RUNNING \`$command\`"
+
+        if [[ -z "$CLIPPIER_DRY_RUN" ]]; then
+            if cargo "${args[@]}" --features "$features_string"; then
+                echo "FINISHED \`$command\`"
+            else
+                >&2 echo "FAILED \`$command\`"
+                return 1
+            fi
+        fi
+
+        for feature in "${all_features[@]}"; do
             local contains=0
 
             for x in "${features[@]}"; do
@@ -48,66 +75,56 @@ function cargo_each_feature_permutation() {
                     contains=1
                 fi
             done
+            for x in "${ignore_features[@]}"; do
+                if [[ "$x" == "$feature" ]]; then
+                    contains=1
+                fi
+            done
 
             if [[ "$contains" == "1" ]]; then
                 continue
             fi
 
-            features+=("$feature")
-            count=$((count + 1))
+            local local_features=("${features[@]}")
+            local_features+=("$feature")
+            local feature_count2=$((feature_count + 1))
+            ignore_feature+=("$feature")
+            ignore_count=$((ignore_count + 1))
 
-            local features_string=""
-
-            for x in "${features[@]}"; do
-                if [[ -n "$features_string" ]]; then
-                    features_string="${features_string},"
-                fi
-                features_string="${features_string}${x}"
-            done
-
-            local command="cargo ${*:$offset} --features \"$features_string\""
-
-            echo "RUNNING \`$command\`"
-
-            if [[ -z "$CLIPPIER_DRY_RUN" ]]; then
-                if cargo "${@:$offset}" --features "$features_string"; then
-                    echo "FINISHED \`$command\`"
-                else
-                    >&2 echo "FAILED \`$command\`"
-                    return 1
-                fi
-            fi
-
-            cargo_each_feature_permutation_inner "$count" "${features[@]}" "${@:$offset}"
-        done <<<"$(moosicbox_clippier Cargo.toml)"
+            cargo_each_feature_permutation_inner "$feature_count2" "${local_features[@]}" "$ignore_count" "${ignore_feature[@]}"
+        done
     }
 
-    while read -r feature; do
-        if [[ "$feature" == "fail-on-warnings" ]]; then
-            continue
+    local command="cargo ${args[*]}"
+
+    echo "RUNNING \`$command\`"
+
+    if [[ -z "$CLIPPIER_DRY_RUN" ]]; then
+        if cargo "${args[@]}"; then
+            echo "FINISHED \`$command\`"
+        else
+            >&2 echo "FAILED \`$command\`"
+            return 1
         fi
+    fi
 
-        local command="cargo ${*} --features \"$feature\""
+    local command="cargo ${args[*]} --features default"
 
-        echo "RUNNING \`$command\`"
+    echo "RUNNING \`$command\`"
 
-        if [[ -z "$CLIPPIER_DRY_RUN" ]]; then
-            if cargo "${@}" --features "$feature"; then
-                echo "FINISHED \`$command\`"
-            else
-                >&2 echo "FAILED \`$command\`"
-                return 1
-            fi
+    if [[ -z "$CLIPPIER_DRY_RUN" ]]; then
+        if cargo "${args[@]}" --features default; then
+            echo "FINISHED \`$command\`"
+        else
+            >&2 echo "FAILED \`$command\`"
+            return 1
         fi
+    fi
 
-        if [[ "$feature" == "default" ]]; then
-            continue
-        fi
-
-        local features=("$feature")
+    for feature in "${all_features[@]}"; do
         ignore+=("$feature")
-        cargo_each_feature_permutation_inner 1 "${features[@]}" "${@}"
-    done <<<"$(moosicbox_clippier Cargo.toml)"
+        cargo_each_feature_permutation_inner 1 "$feature" 0
+    done
 
     echo "done"
 }
