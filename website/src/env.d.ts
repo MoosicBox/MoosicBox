ffdf5deb2fd8856dd8223457e220ff1af94f808a
Braden Steffaniak <BradenSteffaniak@gmail.com>
2024-09-30 21:36:58 -0400

    Fix merge conflict

--- website/src/env.d.ts
+++ website/src/env.d.ts
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
-/// <reference path="../.astro/types.d.ts" />
-||||||| parent of 2b5eeca (Add solidjs website)
-=======
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
->>>>>>> 2b5eeca (Add solidjs website)
