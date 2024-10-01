92681b224700383bda688f8c4e04f00277638e66
Braden Steffaniak <BradenSteffaniak@gmail.com>
2024-10-01 16:23:22 -0400

    Add 'features' subcommand to moosicbox_clippier

--- packages/clippier/src/main.rs
+++ packages/clippier/src/main.rs
@@ -1,4 +1,4 @@
-use clap::{Parser, ValueEnum};
+use clap::{Parser, Subcommand, ValueEnum};
 use toml::Value;
 
 #[derive(ValueEnum, Clone, Copy, Debug, PartialEq, Eq)]
@@ -11,11 +11,19 @@ pub enum OutputType {
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    #[arg(index = 1)]
-    file: String,
+    #[command(subcommand)]
+    cmd: Commands,
+}
 
-    #[arg(short, long, value_enum, default_value_t=OutputType::Raw)]
-    output: OutputType,
+#[derive(Subcommand, Debug, Clone)]
+enum Commands {
+    Features {
+        #[arg(index = 1)]
+        file: String,
+
+        #[arg(short, long, value_enum, default_value_t=OutputType::Raw)]
+        output: OutputType,
+    },
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -23,24 +31,29 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let args = Args::parse();
 
-    log::debug!("Loading file '{}'", args.file);
-    let source = std::fs::read_to_string(args.file)?;
-    let value: Value = toml::from_str(&source)?;
+    match args.cmd {
+        Commands::Features { file, output } => {
+            log::debug!("Loading file '{}'", file);
+            let source = std::fs::read_to_string(file)?;
+            let value: Value = toml::from_str(&source)?;
 
-    if let Some(features) = value.get("features") {
-        if let Some(features) = features.as_table() {
-            match args.output {
-                OutputType::Json => {
-                    println!(
-                        "{}",
-                        serde_json::to_value(features.keys().cloned().collect::<Vec<_>>()).unwrap()
-                    );
-                }
-                OutputType::Raw => {
-                    println!(
-                        "{}",
-                        features.keys().cloned().collect::<Vec<_>>().join("\n")
-                    );
+            if let Some(features) = value.get("features") {
+                if let Some(features) = features.as_table() {
+                    match output {
+                        OutputType::Json => {
+                            println!(
+                                "{}",
+                                serde_json::to_value(features.keys().cloned().collect::<Vec<_>>())
+                                    .unwrap()
+                            );
+                        }
+                        OutputType::Raw => {
+                            println!(
+                                "{}",
+                                features.keys().cloned().collect::<Vec<_>>().join("\n")
+                            );
+                        }
+                    }
                 }
             }
         }
