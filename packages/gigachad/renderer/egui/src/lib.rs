03422694af89122ffc066d7ca722bfac937b0740
Braden Steffaniak <BradenSteffaniak@gmail.com>
2024-10-13 21:57:37 -0400

    #71 Add egui window support

--- packages/gigachad/renderer/egui/src/lib.rs
+++ packages/gigachad/renderer/egui/src/lib.rs
@@ -1,4 +1,132 @@
 #![cfg_attr(feature = "fail-on-warnings", deny(warnings))]
 #![warn(clippy::all, clippy::pedantic, clippy::nursery, clippy::cargo)]
 
+use async_trait::async_trait;
+use eframe::egui;
 pub use gigachad_renderer::*;
+use gigachad_transformer::ContainerElement;
+
+#[derive(Clone)]
+pub struct EguiRenderer {
+    width: Option<u16>,
+    height: Option<u16>,
+    x: Option<i32>,
+    y: Option<i32>,
+}
+
+impl Default for EguiRenderer {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl EguiRenderer {
+    #[must_use]
+    pub const fn new() -> Self {
+        Self {
+            width: None,
+            height: None,
+            x: None,
+            y: None,
+        }
+    }
+}
+
+pub struct EguiRenderRunner {
+    width: u16,
+    height: u16,
+    x: Option<i32>,
+    y: Option<i32>,
+}
+
+impl RenderRunner for EguiRenderRunner {
+    /// # Errors
+    ///
+    /// Will error if FLTK fails to run the event loop.
+    fn run(&mut self) -> Result<(), Box<dyn std::error::Error + Send>> {
+        let mut viewport = egui::ViewportBuilder::default()
+            .with_inner_size([f32::from(self.width), f32::from(self.height)]);
+
+        #[allow(clippy::cast_precision_loss)]
+        if let (Some(x), Some(y)) = (self.x, self.y) {
+            viewport = viewport.with_position((x as f32, y as f32));
+        }
+
+        let options = eframe::NativeOptions {
+            viewport,
+            ..Default::default()
+        };
+
+        log::debug!("run: starting");
+        eframe::run_native(
+            "Image Viewer",
+            options,
+            Box::new(|_cc| Ok(Box::<EguiApp>::default())),
+        )
+        .unwrap();
+        log::debug!("run: finished");
+
+        Ok(())
+    }
+}
+
+#[async_trait]
+impl Renderer for EguiRenderer {
+    /// # Panics
+    ///
+    /// Will panic if elements `Mutex` is poisoned.
+    ///
+    /// # Errors
+    ///
+    /// Will error if FLTK app fails to start
+    async fn init(
+        &mut self,
+        width: u16,
+        height: u16,
+        x: Option<i32>,
+        y: Option<i32>,
+    ) -> Result<(), Box<dyn std::error::Error + Send + 'static>> {
+        self.width = Some(width);
+        self.height = Some(height);
+        self.x = x;
+        self.y = y;
+        Ok(())
+    }
+
+    /// # Errors
+    ///
+    /// Will error if FLTK fails to run the event loop.
+    async fn to_runner(
+        &mut self,
+    ) -> Result<Box<dyn RenderRunner>, Box<dyn std::error::Error + Send>> {
+        Ok(Box::new(EguiRenderRunner {
+            width: self.width.unwrap(),
+            height: self.height.unwrap(),
+            x: self.x,
+            y: self.y,
+        }))
+    }
+
+    /// # Errors
+    ///
+    /// Will error if FLTK fails to render the elements.
+    ///
+    /// # Panics
+    ///
+    /// Will panic if elements `Mutex` is poisoned.
+    fn render(
+        &mut self,
+        elements: ContainerElement,
+    ) -> Result<(), Box<dyn std::error::Error + Send + 'static>> {
+        log::debug!("render: {elements:?}");
+
+        Ok(())
+    }
+}
+
+#[derive(Default)]
+struct EguiApp {}
+
+impl eframe::App for EguiApp {
+    fn update(&mut self, _ctx: &egui::Context, _frame: &mut eframe::Frame) {}
+}
