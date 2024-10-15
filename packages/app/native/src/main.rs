f80b47eef04eda4c8355b7cb440ef3da09afd5fe
Braden Steffaniak <BradenSteffaniak@gmail.com>
2024-10-15 07:30:00 -0400

    #71 Don't show console for windows release builds

--- packages/app/native/src/main.rs
+++ packages/app/native/src/main.rs
@@ -1,3 +1,5 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
 use std::sync::Arc;
 
 use moosicbox_app_native_lib::router::Router;
