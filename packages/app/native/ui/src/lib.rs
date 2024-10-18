f2ee36b8bb1748f5531366971a9e13cff9e485a2
Braden Steffaniak <BradenSteffaniak@gmail.com>
2024-10-18 07:30:00 -0400

    #71 Add more player UI

--- packages/app/native/ui/src/lib.rs
+++ packages/app/native/ui/src/lib.rs
@@ -59,8 +59,61 @@ pub fn sidebar_navigation() -> Markup {
 #[must_use]
 pub fn player() -> Markup {
     html! {
-        div sx-height="100" {
-            ("player")
+        div sx-height="100" sx-dir="row" {
+            div sx-dir="row" {
+                @let size = 70;
+                div sx-width=(size) sx-height=(size) {
+                    (album_cover_img(&ApiLibraryAlbum { album_id: 1, contains_cover: true, ..Default::default() }, size))
+                }
+            }
+            div sx-dir="row" {
+                @let size = 36;
+                div sx-width=(size) sx-height=(size) {
+                    img
+                        sx-width=(size)
+                        sx-height=(size)
+                        src=(image!("../../../../../app-website/public/img/chevron-left-white.svg"));
+                }
+                div sx-width=(size) sx-height=(size) {
+                    img
+                        sx-width=(size)
+                        sx-height=(size)
+                        src=(image!("../../../../../app-website/public/img/pause-button-white.svg"));
+                }
+                div sx-width=(size) sx-height=(size) {
+                    img
+                        sx-width=(size)
+                        sx-height=(size)
+                        src=(image!("../../../../../app-website/public/img/chevron-right-white.svg"));
+                }
+            }
+            div sx-dir="row" {
+                @let size = 25;
+                div sx-width=(size) sx-height=(size) {
+                    img
+                        sx-width=(size)
+                        sx-height=(size)
+                        src=(image!("../../../../../app-website/public/img/audio-white.svg"));
+                }
+                div sx-width=(size) sx-height=(size) {
+                    img
+                        sx-width=(size)
+                        sx-height=(size)
+                        src=(image!("../../../../../app-website/public/img/speaker-white.svg"));
+                }
+                div sx-width=(size) sx-height=(size) {
+                    img
+                        sx-width=(size)
+                        sx-height=(size)
+                        src=(image!("../../../../../app-website/public/img/sessions-white.svg"));
+                }
+                div sx-width=(size) sx-height=(size) {
+                    img
+                        sx-width=(size)
+                        sx-height=(size)
+                        src=(image!("../../../../../app-website/public/img/playlist-white.svg"));
+                }
+            }
         }
     }
 }
@@ -111,19 +164,25 @@ fn album_cover_url(album: &ApiLibraryAlbum, width: u16, height: u16) -> String {
     }
 }
 
-#[must_use]
-pub fn album_page_content(album: ApiAlbum, versions: &[ApiAlbumVersion]) -> Markup {
-    let ApiAlbum::Library(album) = album;
-
-    let size: u16 = 200;
+fn album_cover_img(album: &ApiLibraryAlbum, size: u16) -> Markup {
     #[allow(clippy::cast_sign_loss)]
     #[allow(clippy::cast_possible_truncation)]
     let request_size = (f64::from(size) * 1.33).round() as u16;
 
     html! {
+        img src=(album_cover_url(&album, request_size, request_size)) sx-width=(size) sx-height=(size);
+    }
+}
+
+#[must_use]
+pub fn album_page_content(album: ApiAlbum, versions: &[ApiAlbumVersion]) -> Markup {
+    let ApiAlbum::Library(album) = album;
+
+    html! {
         div sx-dir="row" {
+            @let size = 200;
             div sx-width=(size) sx-height=(size + 30) {
-                img src=(album_cover_url(&album, request_size, request_size)) sx-width=(size) sx-height=(size);
+                (album_cover_img(&album, size))
             }
             div {
                 h1 { (album.title) }
