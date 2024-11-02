b0d1e9490760dda3e1a9728b97370539588977a5
Braden Steffaniak <BradenSteffaniak@gmail.com>
2024-11-02 09:32:55 -0400

    Enable windows for load balancer

--- packages/load_balancer/src/lib.rs
+++ packages/load_balancer/src/lib.rs
@@ -1,7 +1,4 @@
 #![cfg_attr(feature = "fail-on-warnings", deny(warnings))]
 
-#[cfg(not(target_os = "windows"))]
 mod load_balancer;
-
-#[cfg(not(target_os = "windows"))]
 pub use load_balancer::*;
--- packages/load_balancer/src/main.rs
+++ packages/load_balancer/src/main.rs
@@ -1,15 +1,7 @@
 #![cfg_attr(feature = "fail-on-warnings", deny(warnings))]
 
-#[cfg(not(target_os = "windows"))]
 mod server;
 
 fn main() {
-    #[cfg(not(target_os = "windows"))]
-    {
-        server::serve()
-    }
-    #[cfg(target_os = "windows")]
-    {
-        panic!("Windows OS is not supported. Blame the way daemons are designed.")
-    }
+    server::serve()
 }
