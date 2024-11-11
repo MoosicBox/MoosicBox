0fb50cdc57ffa9267747b3869eb0ea51524831da
Braden Steffaniak <BradenSteffaniak@gmail.com>
2024-11-11 07:30:00 -0500

    Add debug log

--- packages/scan/src/local.rs
+++ packages/scan/src/local.rs
@@ -182,6 +182,7 @@ fn scan_track(
             bit_depth,
             channels,
         ) = moosicbox_task::spawn_blocking("scan: scan_track", move || {
+            log::debug!("scan_track: path={path:?}");
             let extension = path
                 .extension()
                 .and_then(std::ffi::OsStr::to_str)
