705cf2ef489e85e689d3b339141be23fd02cabaf
Braden Steffaniak <BradenSteffaniak@gmail.com>
2024-12-02 07:30:00 -0500

    #71 Add links to artist page and album page & make sidebar scrollable

--- packages/app/native/ui/src/play_queue.rs
+++ packages/app/native/ui/src/play_queue.rs
@@ -6,15 +6,29 @@ use moosicbox_core::sqlite::models::ApiTrack;
 use crate::{public_img, state::State};
 
 fn render_play_queue_item(track: &ApiTrack, is_history: bool) -> Markup {
+    let album_page_url = crate::albums::album_page_url(
+        &track.album_id.to_string(),
+        false,
+        Some(track.api_source),
+        Some(track.track_source),
+        track.sample_rate,
+        track.bit_depth,
+    );
+    let artist_page_url = crate::artists::artist_page_url(&track.artist_id.to_string());
     html! {
         div sx-dir="row" sx-opacity=[if is_history { Some(0.5) } else { None }] {
-            (crate::albums::album_cover_img_from_track(track, 50))
+            @let icon_size = 50;
+            a href=(album_page_url) sx-width=(icon_size) sx-height=(icon_size) {
+                (crate::albums::album_cover_img_from_track(track, icon_size))
+            }
             div {
                 div {
-                    (format!("{} - {}", track.title, track.album))
+                    a href=(album_page_url) {
+                        (format!("{} - {}", track.title, track.album))
+                    }
                 }
                 div {
-                    (track.artist)
+                    a href=(artist_page_url) { (track.artist) }
                 }
             }
             @let icon_size = 20;
@@ -66,22 +80,33 @@ pub fn play_queue(state: &State) -> Markup {
             sx-bottom=(170)
             sx-right=(0)
         {
-            h1 { ("Play queue") }
-            @for track in history {
-                (render_play_queue_item(track, true))
-            }
-            ({
-                future.peek().map_or_else(|| html!(), |track| html! {
-                    div sx-dir="row" {
-                        ("Playing from: ")
-                        a href=(crate::albums::album_page_url(&track.album_id.to_string(), false, Some(track.api_source), Some(track.track_source), track.sample_rate, track.bit_depth)) {
-                            (track.album)
+            h1 sx-height=(30) { ("Play queue") }
+            div sx-overflow-y="auto" {
+                @for track in history {
+                    (render_play_queue_item(track, true))
+                }
+                ({
+                    future.peek().map_or_else(|| html!(), |track| html! {
+                        div sx-dir="row" {
+                            ("Playing from: ")
+                            a href=(
+                                crate::albums::album_page_url(
+                                    &track.album_id.to_string(),
+                                    false,
+                                    Some(track.api_source),
+                                    Some(track.track_source),
+                                    track.sample_rate,
+                                    track.bit_depth
+                                )
+                            ) {
+                                (track.album)
+                            }
                         }
-                    }
+                    })
                 })
-            })
-            @for track in future {
-                (render_play_queue_item(track, false))
+                @for track in future {
+                    (render_play_queue_item(track, false))
+                }
             }
         }
     }
