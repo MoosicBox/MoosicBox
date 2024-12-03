8e881f931eb241de59b2dfee08d3ab93cafdf13d
Braden Steffaniak <BradenSteffaniak@gmail.com>
2024-12-03 07:30:00 -0500

    Upgrade app-website dependencies

--- app-website/src/components/ScanStatusBanner/ScanStatusBanner.tsx
+++ app-website/src/components/ScanStatusBanner/ScanStatusBanner.tsx
@@ -28,7 +28,9 @@ export default function scanStatusBannerFunc() {
                             <div class="scan-status-banner-scan-task">
                                 {task.scanned.toLocaleString()} of{' '}
                                 {task.total.toLocaleString()} track
-                                {task.total === 1 ? '' : 's'} scanned
+                                {task.total === 1
+                                    ? ''
+                                    : 's'} scanned
                                 <button
                                     class="remove-button-styles"
                                     onClick={() => hideTask(task.task)}
