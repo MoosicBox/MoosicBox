8cecfc211de5b60052a9295fcb0a4c6ad0042b40
Braden Steffaniak <BradenSteffaniak@gmail.com>
2024-12-06 21:15:59 -0500

    Update SST provider verisons

--- app-website/sst.config.ts
+++ app-website/sst.config.ts
@@ -1,7 +1,5 @@
 /// <reference path="./.sst/platform/config.d.ts" />
-
 import { readdirSync } from 'fs';
-
 export default $config({
     app(input) {
         return {
@@ -9,21 +7,19 @@ export default $config({
             removal: input?.stage === 'prod' ? 'retain' : 'remove',
             home: 'aws',
             providers: {
-                aws: { region: 'us-east-1', version: '^6.52.0' },
-                cloudflare: '^5.40.0',
+                aws: { region: 'us-east-1', version: '6.64.0' },
+                cloudflare: '5.44.0',
             },
         };
     },
     async run() {
         const outputs = {};
-
         for (const value of readdirSync('./infra/')) {
             const result = await import(`./infra/${value}`);
             if (result.outputs) {
                 Object.assign(outputs, result.outputs);
             }
         }
-
         return outputs;
     },
 });
