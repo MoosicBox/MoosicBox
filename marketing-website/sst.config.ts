df41a9cfa5592134d719c869ad11b28b4a34dc50
Braden Steffaniak <BradenSteffaniak@gmail.com>
2024-12-06 21:42:07 -0500

    Update SST provider verisons for marketing-website

--- marketing-website/sst.config.ts
+++ marketing-website/sst.config.ts
@@ -7,8 +7,8 @@ export default $config({
             removal: input?.stage === 'prod' ? 'retain' : 'remove',
             home: 'aws',
             providers: {
-                aws: { region: 'us-east-1', version: '6.52.0' },
-                cloudflare: '5.40.0',
+                aws: { region: 'us-east-1', version: '6.64.0' },
+                cloudflare: '5.44.0',
             },
         };
     },
