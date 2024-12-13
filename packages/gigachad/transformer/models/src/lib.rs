d1c5880d20fe3f7bddccfe51be43a3aaf36556f8
Braden Steffaniak <BradenSteffaniak@gmail.com>
2024-12-13 07:30:00 -0500

    #71 Update kebab-case display for various structs

--- packages/gigachad/transformer/models/src/lib.rs
+++ packages/gigachad/transformer/models/src/lib.rs
@@ -16,8 +16,8 @@ pub enum LayoutDirection {
 impl std::fmt::Display for LayoutDirection {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::Row => f.write_str("Row"),
-            Self::Column => f.write_str("Column"),
+            Self::Row => f.write_str("row"),
+            Self::Column => f.write_str("column"),
         }
     }
 }
@@ -55,7 +55,14 @@ pub enum JustifyContent {
 
 impl std::fmt::Display for JustifyContent {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_fmt(format_args!("{self:?}"))
+        match self {
+            Self::Start => f.write_str("start"),
+            Self::Center => f.write_str("center"),
+            Self::End => f.write_str("end"),
+            Self::SpaceBetween => f.write_str("space-between"),
+            Self::SpaceEvenly => f.write_str("space-evenly"),
+            Self::Default => f.write_str("default"),
+        }
     }
 }
 
@@ -72,7 +79,12 @@ pub enum AlignItems {
 
 impl std::fmt::Display for AlignItems {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_fmt(format_args!("{self:?}"))
+        match self {
+            Self::Start => f.write_str("start"),
+            Self::Center => f.write_str("center"),
+            Self::End => f.write_str("end"),
+            Self::Default => f.write_str("default"),
+        }
     }
 }
 
@@ -175,7 +187,33 @@ pub enum Cursor {
 
 impl std::fmt::Display for Cursor {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_fmt(format_args!("{self:?}"))
+        match self {
+            Self::Auto => f.write_str("auto"),
+            Self::Pointer => f.write_str("pointer"),
+            Self::Text => f.write_str("text"),
+            Self::Crosshair => f.write_str("crosshair"),
+            Self::Move => f.write_str("move"),
+            Self::NotAllowed => f.write_str("not-allowed"),
+            Self::NoDrop => f.write_str("no-drop"),
+            Self::Grab => f.write_str("grab"),
+            Self::Grabbing => f.write_str("grabbing"),
+            Self::AllScroll => f.write_str("all-scroll"),
+            Self::ColResize => f.write_str("col-resize"),
+            Self::RowResize => f.write_str("row-resize"),
+            Self::NResize => f.write_str("n-resize"),
+            Self::EResize => f.write_str("e-resize"),
+            Self::SResize => f.write_str("s-resize"),
+            Self::WResize => f.write_str("w-resize"),
+            Self::NeResize => f.write_str("ne-resize"),
+            Self::NwResize => f.write_str("nw-resize"),
+            Self::SeResize => f.write_str("se-resize"),
+            Self::SwResize => f.write_str("sw-resize"),
+            Self::EwResize => f.write_str("ew-resize"),
+            Self::NsResize => f.write_str("ns-resize"),
+            Self::NeswResize => f.write_str("nesw-resize"),
+            Self::ZoomIn => f.write_str("zoom-in"),
+            Self::ZoomOut => f.write_str("zoom-out"),
+        }
     }
 }
 
@@ -191,7 +229,11 @@ pub enum Position {
 
 impl std::fmt::Display for Position {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_fmt(format_args!("{self:?}"))
+        match self {
+            Self::Static => f.write_str("static"),
+            Self::Relative => f.write_str("relative"),
+            Self::Absolute => f.write_str("absolute"),
+        }
     }
 }
 
@@ -206,6 +248,9 @@ pub enum Visibility {
 
 impl std::fmt::Display for Visibility {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_fmt(format_args!("{self:?}"))
+        match self {
+            Self::Visible => f.write_str("visible"),
+            Self::Hidden => f.write_str("hidden"),
+        }
     }
 }
