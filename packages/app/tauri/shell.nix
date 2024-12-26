a152afe28187bbc1856703e6a9385e499a7871ef
Braden Steffaniak <BradenSteffaniak@gmail.com>
2024-12-26 10:40:13 -0500

    Update to nixpkgs 24.11

--- packages/app/tauri/shell.nix
+++ packages/app/tauri/shell.nix
@@ -1,6 +1,9 @@
 let
-   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
-   pkgs = import nixpkgs { config = {}; overlays = []; };
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.11";
+  pkgs = import nixpkgs {
+    config = { };
+    overlays = [ ];
+  };
 in
 
 pkgs.mkShellNoCC {
