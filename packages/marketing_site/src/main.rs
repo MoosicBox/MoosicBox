b4c4010942a27bfad1837257c9827d99b924ba14
Braden Steffaniak <BradenSteffaniak@gmail.com>
2025-01-29 07:30:00 -0500

    #71 Add support to gen html for static routes

--- packages/marketing_site/src/main.rs
+++ packages/marketing_site/src/main.rs
@@ -6,24 +6,45 @@
 #![cfg_attr(feature = "fail-on-warnings", deny(warnings))]
 #![warn(clippy::all, clippy::pedantic, clippy::nursery, clippy::cargo)]
 
-use std::sync::{Arc, OnceLock};
+use std::{
+    collections::HashMap,
+    path::PathBuf,
+    sync::{Arc, OnceLock},
+};
 
+use clap::{Parser, Subcommand};
 use moosicbox_app_native_lib::{
     renderer::{Color, Renderer},
-    router::Router,
+    router::{ClientInfo, ClientOs, RequestInfo, RouteRequest, Router},
 };
 use moosicbox_env_utils::{default_env_usize, option_env_f32, option_env_i32};
 use moosicbox_logging::free_log_client::DynLayer;
-use tokio::sync::RwLock;
+use tokio::{io::AsyncWriteExt as _, sync::RwLock};
 
 mod download;
 
 static ROUTER: OnceLock<Router> = OnceLock::new();
 static RENDERER: OnceLock<Arc<RwLock<Box<dyn Renderer>>>> = OnceLock::new();
 
-#[cfg(feature = "assets")]
-static ASSETS_DIR: std::sync::LazyLock<Option<std::path::PathBuf>> =
+static DEFAULT_OUTPUT_DIR: &str = "gen";
+static CARGO_MANIFEST_DIR: std::sync::LazyLock<Option<std::path::PathBuf>> =
     std::sync::LazyLock::new(|| std::option_env!("CARGO_MANIFEST_DIR").map(Into::into));
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    cmd: Commands,
+}
+
+#[derive(Subcommand, Debug, Clone, PartialEq, Eq)]
+enum Commands {
+    Gen {
+        #[arg(short, long)]
+        output: Option<String>,
+    },
+    Serve,
+}
 
 #[allow(clippy::too_many_lines)]
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -44,16 +65,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         moosicbox_logging::init(filename, Some(layers)).expect("Failed to initialize FreeLog");
     }
 
-    let threads = default_env_usize("MAX_THREADS", 64).unwrap_or(64);
-    log::debug!("Running with {threads} max blocking threads");
-
-    let runtime = tokio::runtime::Builder::new_multi_thread()
-        .enable_all()
-        .max_blocking_threads(threads)
-        .build()
-        .unwrap();
-
-    let runtime = Arc::new(runtime);
+    let args = Args::parse();
+    log::info!("args={args:?}");
 
     let router = Router::new()
         .with_static_route(&["/", "/home"], |_| async {
@@ -67,6 +80,96 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         });
 
     moosicbox_assert::assert_or_panic!(ROUTER.set(router.clone()).is_ok(), "Already set ROUTER");
+
+    if let Commands::Gen { output } = args.cmd {
+        let runtime = tokio::runtime::Builder::new_multi_thread()
+            .enable_all()
+            .build()
+            .unwrap();
+
+        runtime.block_on(async move {
+            let output = output.unwrap_or_else(|| {
+                CARGO_MANIFEST_DIR
+                    .as_ref()
+                    .and_then(|x| x.join(DEFAULT_OUTPUT_DIR).to_str().map(ToString::to_string))
+                    .unwrap_or_else(|| DEFAULT_OUTPUT_DIR.to_string())
+            });
+            let output_path: PathBuf = output.into();
+            let router = ROUTER.get().unwrap().clone();
+            let static_routes = router.static_routes.read().unwrap().clone();
+
+            for (path, handler) in &static_routes {
+                let path_str = match path {
+                    moosicbox_app_native_lib::router::RoutePath::Literal(path) => path,
+                    moosicbox_app_native_lib::router::RoutePath::Literals(paths) => {
+                        if let Some(path) = paths.first() {
+                            path
+                        } else {
+                            continue;
+                        }
+                    }
+                };
+                let path_str = path_str.strip_prefix('/').unwrap_or(path_str);
+                let path_str = if path_str.is_empty() {
+                    "index"
+                } else {
+                    path_str
+                };
+
+                let req = RouteRequest {
+                    path: path_str.to_string(),
+                    query: HashMap::new(),
+                    info: RequestInfo {
+                        client: Arc::new(ClientInfo {
+                            os: ClientOs {
+                                name: "n/a".to_string(),
+                            },
+                        }),
+                    },
+                };
+
+                match handler(req).await {
+                    Ok(view) => {
+                        let html = view.immediate.to_string();
+                        let output_path = output_path.join(format!("{path_str}.html"));
+                        tokio::fs::create_dir_all(&output_path.parent().unwrap())
+                            .await
+                            .expect("Failed to create dirs");
+
+                        log::debug!("gen path={path_str} -> {output_path:?}\n{html}");
+
+                        let mut file = tokio::fs::File::options()
+                            .truncate(true)
+                            .write(true)
+                            .create(true)
+                            .open(&output_path)
+                            .await
+                            .expect("Failed to open file");
+
+                        file.write_all(html.as_bytes())
+                            .await
+                            .expect("Failed to write file");
+                    }
+                    Err(e) => {
+                        panic!("Failed to fetch route view: {e:?}");
+                    }
+                }
+            }
+        });
+
+        return Ok(());
+    }
+
+    let threads = default_env_usize("MAX_THREADS", 64).unwrap_or(64);
+    log::debug!("Running with {threads} max blocking threads");
+
+    let runtime = tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .max_blocking_threads(threads)
+        .build()
+        .unwrap();
+
+    let runtime = Arc::new(runtime);
 
     let mut app = moosicbox_app_native_lib::NativeAppBuilder::new()
         .with_router(router)
@@ -82,7 +185,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         app = app.with_static_asset_route_result(
             moosicbox_app_native_lib::renderer::assets::StaticAssetRoute {
                 route: "public".to_string(),
-                target: ASSETS_DIR
+                target: CARGO_MANIFEST_DIR
                     .as_ref()
                     .map_or_else(
                         || "public".to_string(),
