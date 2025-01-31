de7278277de88beb04f63829906a7bf92648e9b8
Braden Steffaniak <BradenSteffaniak@gmail.com>
2025-01-31 07:30:00 -0500

    #71 Add SKIP_INFRA environment variable
    This fixes the chicken vs egg problem with the .sst/platform code not
    being installed prior to the GigaChadSite.ts file attempting to import
    them

--- packages/marketing_site/sst.config.ts
+++ packages/marketing_site/sst.config.ts
@@ -14,12 +14,18 @@ export default $config({
     },
     async run() {
         const outputs = {};
+
+        if (process.env.SKIP_INFRA === '1') {
+            return outputs;
+        }
+
         for (const value of readdirSync('./infra/')) {
             const result = await import(`./infra/${value}`);
             if (result.outputs) {
                 Object.assign(outputs, result.outputs);
             }
         }
+
         return outputs;
     },
 });
