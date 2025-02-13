aa4d5c826fbc6d831587ad4d72b5984423b8994e
Braden Steffaniak <BradenSteffaniak@gmail.com>
2025-02-13 07:30:00 -0500

    #71 Add JsonF64 & JsonF32 gens

--- packages/gen/src/serde.rs
+++ packages/gen/src/serde.rs
@@ -11,3 +11,37 @@ impl Arbitrary for JsonValue {
         Self(Value::String(XmlString::arbitrary(g).0))
     }
 }
+
+#[derive(Clone, Debug)]
+pub struct JsonF64(pub f64);
+
+impl Arbitrary for JsonF64 {
+    fn arbitrary(g: &mut Gen) -> Self {
+        loop {
+            let num = f64::arbitrary(g);
+
+            if !num.is_finite() {
+                continue;
+            }
+
+            return Self(num);
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct JsonF32(pub f32);
+
+impl Arbitrary for JsonF32 {
+    fn arbitrary(g: &mut Gen) -> Self {
+        loop {
+            let num = f32::arbitrary(g);
+
+            if !num.is_finite() {
+                continue;
+            }
+
+            return Self(num);
+        }
+    }
+}
