6fb537f9faf5907e2ef434ae7210eec78e8e3a26
Braden Steffaniak <BradenSteffaniak@gmail.com>
2025-02-25 07:30:00 -0500

    #71 Handle `canvas_update` SSE events

--- packages/hyperchad/renderer/vanilla_js/web/src/sse.ts
+++ packages/hyperchad/renderer/vanilla_js/web/src/sse.ts
@@ -1,6 +1,16 @@
 import { htmlToElement, swapDom, triggerHandlers } from './core';
 import { fetchEventSource } from './fetch-event-source';
 
+type CanvasAction =
+    | 'clear'
+    | { strokeColor: { r: number; g: number; b: number; a: number | null } }
+    | { fillRect: [[number, number], [number, number]] };
+
+type CanvasUpdate = {
+    target: string;
+    canvasActions: CanvasAction[];
+};
+
 fetchEventSource('$sse', {
     method: 'GET',
     onopen: async (response: Response) => {
@@ -30,6 +40,56 @@ fetchEventSource('$sse', {
                         });
                     }
                 }
+                break;
+            }
+            case 'canvas_update': {
+                const update = JSON.parse(e.data) as CanvasUpdate;
+
+                const element = document.querySelector(
+                    `canvas#${update.target}`,
+                );
+                if (!element) return;
+
+                const canvas = element as HTMLCanvasElement;
+                const ctx = canvas.getContext('2d');
+                if (!ctx) return;
+
+                const attrWidth = canvas.getAttribute('width');
+                const attrHeight = canvas.getAttribute('height');
+
+                if (!attrWidth || canvas.dataset.vNoWidth === 'true') {
+                    canvas.width = canvas.clientWidth;
+                    canvas.dataset.vNoWidth = 'true';
+                }
+                if (!attrHeight || canvas.dataset.vNoHeight === 'true') {
+                    canvas.height = canvas.clientHeight;
+                    canvas.dataset.vNoHeight = 'true';
+                }
+
+                for (const action of update.canvasActions) {
+                    if (action === 'clear') {
+                        ctx.clearRect(0, 0, canvas.width, canvas.height);
+                        continue;
+                    }
+                    if ('strokeColor' in action) {
+                        const r = action.strokeColor.r;
+                        const g = action.strokeColor.g;
+                        const b = action.strokeColor.b;
+                        const a =
+                            typeof action.strokeColor.a === 'number'
+                                ? action.strokeColor.a
+                                : 1;
+
+                        ctx.fillStyle = `rgba(${r},${g},${b},${a})`;
+                        continue;
+                    }
+                    if ('fillRect' in action) {
+                        const [[x1, y1], [x2, y2]] = action.fillRect;
+                        ctx.fillRect(x1, y1, x2 - x1, y2 - y1);
+                        continue;
+                    }
+                }
+
                 break;
             }
         }
