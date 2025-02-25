02bb8c3c3750ba3ffeb1c881b66c504ca05f7be7
Braden Steffaniak <BradenSteffaniak@gmail.com>
2025-02-25 07:30:00 -0500

    #71 Add optional `serde` feature to serialize canvas objects

--- packages/hyperchad/renderer/src/canvas.rs
+++ packages/hyperchad/renderer/src/canvas.rs
@@ -3,9 +3,13 @@
 use hyperchad_color::Color;
 
 #[derive(Debug, Clone, Copy)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(feature = "serde", serde(rename_all = "camelCase"))]
 pub struct Pos(pub f32, pub f32);
 
 #[derive(Debug, Clone)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(feature = "serde", serde(rename_all = "camelCase"))]
 pub enum CanvasAction {
     StrokeSize(f32),
     StrokeColor(Color),
@@ -15,6 +19,8 @@ pub enum CanvasAction {
 }
 
 #[derive(Default, Debug, Clone)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(feature = "serde", serde(rename_all = "camelCase"))]
 pub struct CanvasUpdate {
     pub target: String,
     pub canvas_actions: Vec<CanvasAction>,
