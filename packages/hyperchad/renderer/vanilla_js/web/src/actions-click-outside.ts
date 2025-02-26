1e7c8e29639026ee8d5b2aa6644fd0246baf3277
Braden Steffaniak <BradenSteffaniak@gmail.com>
2025-02-26 07:30:00 -0500

    #71 Fix potential event listener leak

--- packages/hyperchad/renderer/vanilla_js/web/src/actions-click-outside.ts
+++ packages/hyperchad/renderer/vanilla_js/web/src/actions-click-outside.ts
@@ -1,13 +1,32 @@
 import { evaluate } from './actions';
 import { handleError, onAttr } from './core';
 
-onAttr('v-onclickoutside', ({ element, attr }) => {
-    // FIXME: unsubscribe from this when element detached
-    document.addEventListener('click', (event: MouseEvent) => {
-        if (event.target && !element.contains(event.target as Node)) {
-            handleError('onclickoutside', () =>
-                evaluate(attr, { element, event }),
-            );
+const clickOutsideElements: {
+    element: HTMLElement;
+    f: (event: Event) => void;
+}[] = [];
+
+document.addEventListener('click', (event: MouseEvent) => {
+    for (let i = clickOutsideElements.length - 1; i >= 0; i--) {
+        const { element, f } = clickOutsideElements[i];
+        if (!document.contains(element)) {
+            clickOutsideElements.splice(i, 1);
+            continue;
         }
+
+        f(event);
+    }
+});
+
+onAttr('v-onclickoutside', ({ element, attr }) => {
+    clickOutsideElements.push({
+        element,
+        f: (event) => {
+            if (event.target && !element.contains(event.target as Node)) {
+                handleError('onclickoutside', () =>
+                    evaluate(attr, { element, event }),
+                );
+            }
+        },
     });
 });
--- packages/hyperchad/renderer/vanilla_js/web/src/actions-resize.ts
+++ packages/hyperchad/renderer/vanilla_js/web/src/actions-resize.ts
@@ -1,25 +1,43 @@
 import { evaluate } from './actions';
 import { handleError, onAttr } from './core';
 
+const resizeElements: { element: HTMLElement; f: (event: Event) => void }[] =
+    [];
+
+window.addEventListener('resize', (event) => {
+    for (let i = resizeElements.length - 1; i >= 0; i--) {
+        const { element, f } = resizeElements[i];
+        if (!document.contains(element)) {
+            resizeElements.splice(i, 1);
+            continue;
+        }
+
+        f(event);
+    }
+});
+
 onAttr('v-onresize', ({ element, attr }) => {
     let lastWidth = element.clientWidth;
     let lastHeight = element.clientHeight;
 
-    // FIXME: unsubscribe from this when element detached
-    window.addEventListener('resize', (event) => {
-        let resized = false;
+    resizeElements.push({
+        element,
+        f: (event) => {
+            // FIXME: unsubscribe from this when element detached
+            let resized = false;
 
-        if (element.clientWidth !== lastWidth) {
-            resized = true;
-            lastWidth = element.clientWidth;
-        }
-        if (element.clientHeight !== lastHeight) {
-            resized = true;
-            lastHeight = element.clientHeight;
-        }
+            if (element.clientWidth !== lastWidth) {
+                resized = true;
+                lastWidth = element.clientWidth;
+            }
+            if (element.clientHeight !== lastHeight) {
+                resized = true;
+                lastHeight = element.clientHeight;
+            }
 
-        if (!resized) return;
+            if (!resized) return;
 
-        handleError('onresize', () => evaluate(attr, { element, event }));
+            handleError('onresize', () => evaluate(attr, { element, event }));
+        },
     });
 });
