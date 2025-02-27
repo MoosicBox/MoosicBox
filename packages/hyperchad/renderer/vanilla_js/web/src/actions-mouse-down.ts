c9ea651f325503f024a315b657809245fd7ee29f
Braden Steffaniak <BradenSteffaniak@gmail.com>
2025-02-27 07:30:00 -0500

    #71 Handle mousedown event repitition

--- packages/hyperchad/renderer/vanilla_js/web/src/actions-mouse-down.ts
+++ packages/hyperchad/renderer/vanilla_js/web/src/actions-mouse-down.ts
@@ -1,8 +1,35 @@
 import { evaluate } from './actions';
 import { handleError, onAttr } from './core';
 
+let mousePos: { x: number; y: number } | undefined;
+const intervals: Set<ReturnType<typeof setInterval>> = new Set();
+
+window.addEventListener('mousemove', (event: MouseEvent) => {
+    if (!mousePos) mousePos = {} as unknown as typeof mousePos;
+    mousePos!.x = event.clientX;
+    mousePos!.y = event.clientY;
+});
+window.addEventListener('mouseup', () => {
+    intervals.forEach(clearInterval);
+    intervals.clear();
+});
+
 onAttr('v-onmousedown', ({ element, attr }) => {
     element.onmousedown = (event) => {
-        handleError('onmousedown', () => evaluate(attr, { element, event }));
+        const pos: { clientX: number; clientY: number } = {
+            clientX: event.clientX,
+            clientY: event.clientY,
+        };
+        intervals.add(
+            setInterval(() => {
+                if (mousePos) {
+                    pos.clientX = mousePos.x;
+                    pos.clientY = mousePos.y;
+                }
+                handleError('onmousedown', () =>
+                    evaluate(attr, { element, event: pos as unknown as Event }),
+                );
+            }, 16),
+        );
     };
 });
