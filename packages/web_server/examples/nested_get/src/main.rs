7099f5d50cc9162cf71114d3179534d02cdc01b4
Braden Steffaniak <BradenSteffaniak@gmail.com>
2025-04-12 09:28:43 -0400

    Add nested_get web_server example

---
+++ packages/web_server/examples/nested_get/src/main.rs
@@ -0,0 +1,30 @@
+use moosicbox_web_server::{HttpResponse, Scope};
+
+#[tokio::main]
+async fn main() {
+    let cors = moosicbox_web_server::cors::Cors::default()
+        .allow_any_origin()
+        .allow_any_method()
+        .allow_any_header()
+        .expose_any_header();
+
+    let server = moosicbox_web_server::WebServerBuilder::new()
+        .with_cors(cors)
+        .with_scope(Scope::new("/nested").with_route(GET_EXAMPLE))
+        .build_actix();
+
+    server.start().await;
+}
+
+moosicbox_web_server::route!(GET, example, "/example", |req| {
+    Box::pin(async move {
+        Ok(HttpResponse {
+            body: format!(
+                "hello, world! path={} query={}",
+                req.path(),
+                req.query_string()
+            )
+            .into(),
+        })
+    })
+});
