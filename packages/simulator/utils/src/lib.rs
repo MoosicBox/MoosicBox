d390d6ce301f45ea964f364d2c36659f21566967
Braden Steffaniak <BradenSteffaniak@gmail.com>
2025-04-15 07:30:00 -0400

    Add SEED to simulator_utils

--- packages/simulator/utils/src/lib.rs
+++ packages/simulator/utils/src/lib.rs
@@ -2,3 +2,11 @@
 #![warn(clippy::all, clippy::pedantic, clippy::nursery, clippy::cargo)]
 #![allow(clippy::multiple_crate_versions)]
 
+use std::sync::LazyLock;
+
+pub static SEED: LazyLock<u64> = LazyLock::new(|| {
+    std::env::var("SIMULATOR_SEED")
+        .ok()
+        .and_then(|x| x.parse::<u64>().ok())
+        .unwrap_or_else(|| getrandom::u64().unwrap())
+});
