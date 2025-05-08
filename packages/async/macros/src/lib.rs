62c1dc0c6fcb51e7d6722070dd36e7bc7b65d53d
Braden Steffaniak <BradenSteffaniak@gmail.com>
2025-05-08 00:42:15 -0400

    Fix async_macros task yield_now import

--- packages/async/macros/src/lib.rs
+++ packages/async/macros/src/lib.rs
@@ -20,7 +20,7 @@ impl VisitMut for YieldInjector {
             let base = (*expr_await.base).clone();
             *expr = syn::parse_quote!({
                 let __yield_res = #base.await;
-                ::switchy_async::task::yield_now().await;
+                ::switchy::unsync::task::yield_now().await;
                 __yield_res
             });
         }
