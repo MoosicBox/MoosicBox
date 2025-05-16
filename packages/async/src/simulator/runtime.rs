bb0f20bc674630580db5a1341524a797a8d6abc8
Braden Steffaniak <BradenSteffaniak@gmail.com>
2025-05-16 07:30:00 -0400

    Add "id" to Task Debug output

--- packages/async/src/simulator/runtime.rs
+++ packages/async/src/simulator/runtime.rs
@@ -367,6 +367,7 @@ struct Task {
 impl std::fmt::Debug for Task {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("Task")
+            .field("id", &self.id)
             .field("finished", &self.finished)
             .field("block", &self.block)
             .finish_non_exhaustive()
