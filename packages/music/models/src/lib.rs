2c0b016a5dd4aa1cb33b0eedca49a6c11aa1f5bc
Braden Steffaniak <BradenSteffaniak@gmail.com>
2025-05-18 12:44:44 -0400

    Impl Display for AlbumSource

--- packages/music/models/src/lib.rs
+++ packages/music/models/src/lib.rs
@@ -541,6 +541,12 @@ impl FromStr for AlbumSource {
     }
 }
 
+impl std::fmt::Display for AlbumSource {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(self.as_ref())
+    }
+}
+
 #[derive(Debug, Serialize, Deserialize, Copy, Clone, Eq, PartialEq)]
 pub enum AlbumSort {
     ArtistAsc,
