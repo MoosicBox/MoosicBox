9e6699788859747486c91c2d51f3bc16ccfbf527
Braden Steffaniak <BradenSteffaniak@gmail.com>
2025-05-26 07:30:00 -0400

    #71 Use RouteRequest::parse_body

--- packages/hyperchad/renderer/html/http/src/actions.rs
+++ packages/hyperchad/renderer/html/http/src/actions.rs
@@ -17,11 +17,13 @@ pub fn handle_action(
     tx: &flume::Sender<(String, Option<Value>)>,
     req: &RouteRequest,
 ) -> Result<Response<Vec<u8>>, Error> {
-    let Some(body) = req.body.clone() else {
-        return Ok(Response::builder().status(400).body(vec![])?);
+    let action = match req.parse_body::<ActionPayload>() {
+        Ok(action) => action,
+        Err(e) => {
+            log::error!("Failed to parse body: {e:?}");
+            return Ok(Response::builder().status(400).body(vec![])?);
+        }
     };
-
-    let action: ActionPayload = serde_json::from_slice(&body)?;
 
     log::debug!("handle_action: action={action:?}");
     tx.send((serde_json::to_string(&action.action).unwrap(), action.value))
