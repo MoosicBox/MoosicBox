cac7f825ff67f673ad0aa0f98e48ddfdd9e6cc2c
Braden Steffaniak <BradenSteffaniak@gmail.com>
2025-05-27 07:30:00 -0400

    #71 Use global 'submit' listener for form.ts plugin

--- packages/hyperchad/renderer/vanilla_js/web/src/form.ts
+++ packages/hyperchad/renderer/vanilla_js/web/src/form.ts
@@ -1,31 +1,36 @@
-import { onElement } from './core';
 import { processRoute } from './routing';
 
-onElement(({ element }) => {
-    if (!(element instanceof HTMLFormElement)) return;
+document.addEventListener('DOMContentLoaded', () => {
+    document.body.addEventListener(
+        'submit',
+        (e) => {
+            const element = e.target as HTMLElement;
 
-    element.addEventListener('submit', (e) => {
-        e.preventDefault();
+            if (!(element instanceof HTMLFormElement)) return;
 
-        const formData = new FormData();
+            let form = e.target as HTMLFormElement | null;
 
-        let form: HTMLFormElement | null = element;
+            e.preventDefault();
 
-        while (form) {
-            const current = new FormData(form);
+            const formData = new FormData();
 
-            for (const pair of current.entries()) {
-                formData.append(pair[0], pair[1]);
+            while (form) {
+                const current = new FormData(form);
+
+                for (const pair of current.entries()) {
+                    formData.append(pair[0], pair[1]);
+                }
+
+                // TODO: Add support for no inheritance form
+                form = form.parentElement?.closest('form') ?? null;
             }
 
-            // TODO: Add support for no inheritance form
-            form = form.parentElement?.closest('form') ?? null;
-        }
+            processRoute(element, {
+                body: formData,
+            });
 
-        processRoute(element, {
-            body: formData,
-        });
-
-        return false;
-    });
+            return false;
+        },
+        true,
+    );
 });
