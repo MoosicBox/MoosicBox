43f836da72ed40984c975872ab907f578fbc7ab4
Braden Steffaniak <BradenSteffaniak@gmail.com>
2025-05-28 07:30:00 -0400

    Update docker image caching

--- terraform/docker.tf
+++ terraform/docker.tf
@@ -7,15 +7,20 @@ resource "docker_image" "load_balancer" {
     platform   = "linux/amd64"
     build_args = {
       BUILDKIT_INLINE_CACHE = "1"
-      DOCKER_BUILDKIT = "1"
+      DOCKER_BUILDKIT       = "1"
+      BUILDKIT_CONTEXT_KEEP_GIT_DIR = "1"
     }
     no_cache = false
     remove = true
     force_remove = true
+    cache_from = [local.image_names.load_balancer]
   }
 
   triggers = {
+    # Hash of all files in the load_balancer directory
     dir_sha1 = sha1(join("", [for f in fileset("${path.root}/../packages/load_balancer", "**"): filesha1("${path.root}/../packages/load_balancer/${f}")]))
+    # Hash of the Dockerfile itself
+    dockerfile_sha1 = filesha1("${path.root}/../packages/load_balancer/LoadBalancer.Dockerfile")
   }
 
   provisioner "local-exec" {
@@ -39,15 +44,20 @@ resource "docker_image" "tunnel_server" {
     platform   = "linux/amd64"
     build_args = {
       BUILDKIT_INLINE_CACHE = "1"
-      DOCKER_BUILDKIT = "1"
+      DOCKER_BUILDKIT       = "1"
+      BUILDKIT_CONTEXT_KEEP_GIT_DIR = "1"
     }
     no_cache = false
     remove = true
     force_remove = true
+    cache_from = [local.image_names.tunnel_server]
   }
 
   triggers = {
+    # Hash of all files in the tunnel_server directory
     dir_sha1 = sha1(join("", [for f in fileset("${path.root}/../packages/tunnel_server", "**"): filesha1("${path.root}/../packages/tunnel_server/${f}")]))
+    # Hash of the Dockerfile itself
+    dockerfile_sha1 = filesha1("${path.root}/../packages/tunnel_server/TunnelServer.Dockerfile")
   }
 
   provisioner "local-exec" {
