ccd1fee8eba30e12928db390b1e3a6fc2a11f3a8
Braden Steffaniak <BradenSteffaniak@gmail.com>
2025-08-05 07:30:00 -0400

    #71 Use switchy_async_macros select!

--- packages/async/src/simulator/mod.rs
+++ packages/async/src/simulator/mod.rs
@@ -12,7 +12,7 @@ pub mod time;
 pub mod util;
 
 #[cfg(feature = "macros")]
-pub use ::futures::select;
+pub use switchy_async_macros::select;
 
 #[cfg(feature = "macros")]
 #[cfg(test)]
@@ -42,9 +42,87 @@ mod test {
             let runtime = build_runtime(&Builder::new()).unwrap();
 
             runtime.block_on(async move {
-                super::select! {
+                crate::select! {
                     () = super::time::sleep(Duration::from_millis(10)) => {},
                 }
+            });
+
+            runtime.wait().unwrap();
+        });
+    }
+
+    #[cfg(feature = "time")]
+    #[test_log::test]
+    fn can_select_future_with_auto_fusing() {
+        switchy_time::simulator::with_real_time(|| {
+            let runtime = build_runtime(&Builder::new()).unwrap();
+
+            runtime.block_on(async move {
+                // Test that our custom select! macro auto-fuses futures
+                let sleep_future = super::time::sleep(Duration::from_millis(10));
+                crate::select! {
+                    () = sleep_future => {},
+                }
+            });
+
+            runtime.wait().unwrap();
+        });
+    }
+
+    #[cfg(feature = "time")]
+    #[test_log::test]
+    fn can_select_with_stream_like_future() {
+        use futures::{StreamExt, stream};
+
+        switchy_time::simulator::with_real_time(|| {
+            let runtime = build_runtime(&Builder::new()).unwrap();
+
+            runtime.block_on(async move {
+                // Test that our custom select! macro works with stream-like futures
+                let mut stream = Box::new(stream::iter(vec![1, 2, 3]));
+                let timeout = super::time::sleep(Duration::from_millis(100));
+
+                crate::select! {
+                    item = stream.next() => {
+                        assert_eq!(item, Some(1));
+                    },
+                    () = timeout => {
+                        panic!("Should have selected stream item");
+                    },
+                }
+            });
+
+            runtime.wait().unwrap();
+        });
+    }
+
+    #[cfg(feature = "time")]
+    #[test_log::test]
+    fn can_select_with_complex_patterns() {
+        use futures::{StreamExt, stream};
+
+        switchy_time::simulator::with_real_time(|| {
+            let runtime = build_runtime(&Builder::new()).unwrap();
+
+            runtime.block_on(async move {
+                // Test complex patterns like the ones used in stream_utils
+                let mut stream = Box::new(stream::iter(vec![Ok::<i32, &str>(42)]));
+                let timeout1 = super::time::sleep(Duration::from_millis(100));
+                let timeout2 = super::time::sleep(Duration::from_millis(200));
+
+                let result = crate::select! {
+                    item = stream.next() => item,
+                    () = timeout1 => {
+                        log::debug!("Timeout 1");
+                        None
+                    }
+                    () = timeout2 => {
+                        log::debug!("Timeout 2");
+                        None
+                    }
+                };
+
+                assert_eq!(result, Some(Ok(42)));
             });
 
             runtime.wait().unwrap();
@@ -58,7 +136,7 @@ mod test {
             let runtime = build_runtime(&Builder::new()).unwrap();
 
             runtime.block_on(async move {
-                super::select! {
+                crate::select! {
                     () = super::time::sleep(Duration::from_millis(10)) => {},
                     () = super::time::sleep(Duration::from_millis(20)) => {
                         panic!("Should have selected other future");
@@ -77,7 +155,7 @@ mod test {
             let runtime = build_runtime(&Builder::new()).unwrap();
 
             runtime.block_on(async move {
-                super::select! {
+                crate::select! {
                     () = super::time::sleep(Duration::from_millis(10)) => {},
                     () = super::time::sleep(Duration::from_millis(20)) => {
                         panic!("Should have selected other future");
@@ -86,7 +164,7 @@ mod test {
             });
 
             runtime.block_on(async move {
-                super::select! {
+                crate::select! {
                     () = super::time::sleep(Duration::from_millis(20)) => {
                         panic!("Should have selected other future");
                     },
@@ -105,7 +183,7 @@ mod test {
             let runtime = build_runtime(&Builder::new()).unwrap();
 
             runtime.block_on(async move {
-                super::select! {
+                crate::select! {
                     () = super::time::sleep(Duration::from_millis(1)) => {},
                     () = super::time::sleep(Duration::from_millis(10)) => {
                         panic!("Should have selected other future");
@@ -117,7 +195,7 @@ mod test {
             });
 
             runtime.block_on(async move {
-                super::select! {
+                crate::select! {
                     () = super::time::sleep(Duration::from_millis(1)) => {},
                     () = super::time::sleep(Duration::from_millis(20)) => {
                         panic!("Should have selected other future");
@@ -129,7 +207,7 @@ mod test {
             });
 
             runtime.block_on(async move {
-                super::select! {
+                crate::select! {
                     () = super::time::sleep(Duration::from_millis(20)) => {
                         panic!("Should have selected other future");
                     },
@@ -141,7 +219,7 @@ mod test {
             });
 
             runtime.block_on(async move {
-                super::select! {
+                crate::select! {
                     () = super::time::sleep(Duration::from_millis(20)) => {
                         panic!("Should have selected other future");
                     },
