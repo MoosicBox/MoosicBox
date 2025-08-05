de74b6c8236d22f7c81bd2dfebaedbd957056dfe
Braden Steffaniak <BradenSteffaniak@gmail.com>
2025-08-04 20:49:46 -0400

    #71 Use switchy instead of tokio directly

--- packages/web_server/examples/simple_get/src/main.rs
+++ packages/web_server/examples/simple_get/src/main.rs
@@ -1,19 +1,24 @@
 use moosicbox_web_server::{HttpResponse, Scope};
 
-#[tokio::main]
-async fn main() {
-    let cors = moosicbox_web_server::cors::Cors::default()
-        .allow_any_origin()
-        .allow_any_method()
-        .allow_any_header()
-        .expose_any_header();
+fn main() {
+    let rt = switchy::unsync::runtime::Runtime::new();
 
-    let server = moosicbox_web_server::WebServerBuilder::new()
-        .with_cors(cors)
-        .with_scope(Scope::new("").with_route(GET_EXAMPLE))
-        .build();
+    let handle = rt.handle();
 
-    server.start().await;
+    handle.block_on(async {
+        let cors = moosicbox_web_server::cors::Cors::default()
+            .allow_any_origin()
+            .allow_any_method()
+            .allow_any_header()
+            .expose_any_header();
+
+        let server = moosicbox_web_server::WebServerBuilder::new()
+            .with_cors(cors)
+            .with_scope(Scope::new("").with_route(GET_EXAMPLE))
+            .build();
+
+        server.start().await;
+    });
 }
 
 moosicbox_web_server::route!(GET, example, "/example", |req| {
