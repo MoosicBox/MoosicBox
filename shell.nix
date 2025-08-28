b8b1bad56423cac42b340a0f9fd333c8245a6424
Braden Steffaniak <BradenSteffaniak@gmail.com>
2025-08-27 22:01:38 -0400

    Update shell.nix to work cross-platform (linux/macos)

--- shell.nix
+++ shell.nix
@@ -3,13 +3,9 @@ let
     config = { };
     overlays = [ ];
   };
-in
 
-pkgs.mkShellNoCC {
-  buildInputs = with pkgs; [
-    fontconfig
-  ];
-  packages = with pkgs; [
+  # Common packages that work on all platforms
+  commonPackages = with pkgs; [
     pkg-config
     gnumake
     gcc
@@ -19,9 +15,14 @@ pkgs.mkShellNoCC {
     libtool
     cmake
     openssl
+    postgresql
+    vips
+  ];
+
+  # Linux-specific packages
+  linuxPackages = with pkgs; [
     alsa-lib
     alsa-lib.dev
-
     alsa-utils
     udev.dev
     wayland
@@ -47,55 +48,97 @@ pkgs.mkShellNoCC {
     cairo
     gdk-pixbuf
     glib
-
     at-spi2-atk # for one example (file dialog)
-    gdk-pixbuf # for one example (file dialog)
     gtkd
     gtk3.dev
     gtk3-x11
     gtk3-x11.dev
     gsettings-desktop-schemas
-    glib
-
-    vips
-    postgresql
     pulseaudio
   ];
 
-  shellHook = ''
-    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath [ pkgs.wayland ]}"
-    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath [ pkgs.libxkbcommon ]}"
-    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath [ pkgs.amdvlk ]}"
-    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath [ pkgs.webkitgtk_4_1 ]}"
-    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath [ pkgs.xorg.libX11 ]}"
-    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath [ pkgs.xorg.libXcursor ]}"
-    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath [ pkgs.xorg.libXfixes ]}"
-    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath [ pkgs.xorg.libXinerama ]}"
-    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath [ pkgs.xorg.libxcb ]}"
-    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath [ pkgs.xorg.xcbutil ]}"
-    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath [ pkgs.xorg.xcbutilimage ]}"
-    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath [ pkgs.xorg.xcbutilkeysyms ]}"
-    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath [ pkgs.xorg.xcbutilwm ]}"
-    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath [ pkgs.fontconfig ]}"
-    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath [ pkgs.at-spi2-atk ]}"
-    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath [ pkgs.gdk-pixbuf ]}"
-    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath [ pkgs.gtkd ]}"
-    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath [ pkgs.gtk3.dev ]}"
-    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath [ pkgs.gtk3-x11 ]}"
-    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath [ pkgs.gtk3-x11.dev ]}"
-    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath [ pkgs.openssl ]}"
-    export RUSTFLAGS="$RUSTFLAGS -C link-arg=-Wl,-rpath,"
-    export RUSTFLAGS="$RUSTFLAGS:${pkgs.lib.makeLibraryPath [ pkgs.wayland ]}"
-    export RUSTFLAGS="$RUSTFLAGS:${pkgs.lib.makeLibraryPath [ pkgs.libxkbcommon ]}"
-    export RUSTFLAGS="$RUSTFLAGS:${pkgs.lib.makeLibraryPath [ pkgs.webkitgtk_4_1 ]}"
-    export RUSTFLAGS="$RUSTFLAGS:${pkgs.lib.makeLibraryPath [ pkgs.gtk3 ]}"
-    export RUSTFLAGS="$RUSTFLAGS:${pkgs.lib.makeLibraryPath [ pkgs.vulkan-loader ]}"
-    export RUSTFLAGS="$RUSTFLAGS:${pkgs.lib.makeLibraryPath [ pkgs.pango ]}"
-    export RUSTFLAGS="$RUSTFLAGS:${pkgs.lib.makeLibraryPath [ pkgs.cairo ]}"
-    export RUSTFLAGS="$RUSTFLAGS:${pkgs.lib.makeLibraryPath [ pkgs.gdk-pixbuf ]}"
-    export RUSTFLAGS="$RUSTFLAGS:${pkgs.lib.makeLibraryPath [ pkgs.libsoup_3 ]}"
-    export RUSTFLAGS="$RUSTFLAGS:${pkgs.lib.makeLibraryPath [ pkgs.glib ]}"
-    export RUSTFLAGS="$RUSTFLAGS:${pkgs.lib.makeLibraryPath [ pkgs.alsa-lib ]}"
+  # macOS-specific packages
+  darwinPackages = with pkgs; [
+    darwin.apple_sdk.frameworks.CoreAudio
+    darwin.apple_sdk.frameworks.AudioUnit
+    darwin.apple_sdk.frameworks.AudioToolbox
+    darwin.apple_sdk.frameworks.CoreFoundation
+    darwin.apple_sdk.frameworks.CoreServices
+    darwin.apple_sdk.frameworks.Security
+    darwin.apple_sdk.frameworks.SystemConfiguration
+    portaudio
+  ];
+
+  # Platform-specific shell hooks
+  linuxShellHook = ''
+    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${
+      pkgs.lib.makeLibraryPath [
+        pkgs.wayland
+        pkgs.libxkbcommon
+        pkgs.webkitgtk_4_1
+        pkgs.xorg.libX11
+        pkgs.xorg.libXcursor
+        pkgs.xorg.libXfixes
+        pkgs.xorg.libXinerama
+        pkgs.xorg.libxcb
+        pkgs.xorg.xcbutil
+        pkgs.xorg.xcbutilimage
+        pkgs.xorg.xcbutilkeysyms
+        pkgs.xorg.xcbutilwm
+        pkgs.fontconfig
+        pkgs.at-spi2-atk
+        pkgs.gdk-pixbuf
+        pkgs.gtkd
+        pkgs.gtk3.dev
+        pkgs.gtk3-x11
+        pkgs.gtk3-x11.dev
+        pkgs.openssl
+      ]
+    }"
+    export RUSTFLAGS="$RUSTFLAGS -C link-arg=-Wl,-rpath,:${
+      pkgs.lib.makeLibraryPath [
+        pkgs.wayland
+        pkgs.libxkbcommon
+        pkgs.webkitgtk_4_1
+        pkgs.gtk3
+        pkgs.vulkan-loader
+        pkgs.pango
+        pkgs.cairo
+        pkgs.gdk-pixbuf
+        pkgs.libsoup_3
+        pkgs.glib
+        pkgs.alsa-lib
+      ]
+    }"
   '';
 
+  darwinShellHook = ''
+    export DYLD_LIBRARY_PATH="$DYLD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath [ pkgs.openssl ]}"
+    export RUSTFLAGS="$RUSTFLAGS -C link-arg=-Wl,-rpath,${pkgs.lib.makeLibraryPath [ pkgs.openssl ]}"
+  '';
+
+in
+
+pkgs.mkShellNoCC {
+  buildInputs = with pkgs; [
+    fontconfig
+  ];
+  packages =
+    commonPackages
+    ++ (
+      if pkgs.stdenv.isDarwin then
+        darwinPackages
+      else if pkgs.stdenv.isLinux then
+        linuxPackages
+      else
+        [ ]
+    );
+
+  shellHook =
+    if pkgs.stdenv.isDarwin then
+      darwinShellHook
+    else if pkgs.stdenv.isLinux then
+      linuxShellHook
+    else
+      "";
 }
