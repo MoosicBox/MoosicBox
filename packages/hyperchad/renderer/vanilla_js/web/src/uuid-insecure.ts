a12a6e017f28615a19eff22c27cd2645d3c99c1f
MoosicBoxBot <MoosicBoxBot@gmail.com>
2025-11-28 04:17:07 +0000

    Upgrade hyperchad vanilla-js renderer node dependencies

--- packages/hyperchad/renderer/vanilla_js/web/src/uuid-insecure.ts
+++ packages/hyperchad/renderer/vanilla_js/web/src/uuid-insecure.ts
@@ -12,10 +12,10 @@ v.genUuid = function () {
         function (c) {
             let r = Math.random() * 16;
             if (d > 0) {
-                r = (d + r) % 16 | 0;
+                r = ((d + r) % 16) | 0;
                 d = Math.floor(d / 16);
             } else {
-                r = (d2 + r) % 16 | 0;
+                r = ((d2 + r) % 16) | 0;
                 d2 = Math.floor(d2 / 16);
             }
             return (c === 'x' ? r : (r & 0x3) | 0x8).toString(16);
